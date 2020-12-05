c83d041b6fc02c2c3171c3df7d0ea70ce6f4b3d4
objektwerks <objektwerks@runbox.com>
2020-12-05 07:56:08 -0500

    refactored big O methods and tests

--- src/main/scala/objektwerks/BigO.scala
+++ src/main/scala/objektwerks/BigO.scala
@@ -1,31 +1,29 @@
 package objektwerks
 
 import scala.annotation.tailrec
+import scala.collection.mutable.ArrayBuffer
 
 object BigO {
   // O(1) - Constant Time
-  def constantTime(array: Array[Int]): Int = {
-    array(0)
-  }
+  def constantTimeSearch(array: Array[Int], index: Int): Int = array(index)
 
   // O(n) - Linear Time
-  def linearTime(array: Array[Int]): Int = {
+  def linearTimeSum(array: Array[Int]): Int = {
     var sum = 0
-    for (i <- array) {
-      sum = sum + i
-    }
+    for (i <- array) { sum = sum + i }
     sum
   }
 
   // O(log N) - Binary Search
   case class Value(number: Int)
   implicit def ordering: Ordering[Value] = Ordering.by(_.number)
+  def binarySearch(buffer: ArrayBuffer[Value], value: Value): Int = buffer.search(value).insertionPoint
 
   // O(n^2) - Quadratic Time
-  def quadraticTime(): Array[Array[Int]] = {
+  def quadraticTimeMatrix(): Array[Array[Int]] = {
     val matrix = Array.ofDim[Int](3, 3)
-    for (i <- 0 to matrix.length - 1) {
-      for (j <- 0 to matrix.length - 1) {
+    for (i <- 0 until matrix.length - 1) {
+      for (j <- 0 until matrix.length - 1) {
         matrix(i)(j) = j
       }
     }
@@ -33,7 +31,7 @@ object BigO {
   }
 
   // O(2^N) - Exponential Time
-  def fibonacci(n: Long): BigInt = {
+  def exponentialTimeFibonacci(n: Long): BigInt = {
     @tailrec
     def loop(n: Long, a: Long, b: Long): BigInt = n match {
       case 0 => a
@@ -44,16 +42,16 @@ object BigO {
 
   // O(N!) - Factorial Time
   @tailrec
-  final def intersectLists[A](listA: List[A],
-                              listB: List[A],
-                              acc: List[A] = List.empty[A]): List[A] =
+  final def factorialTimeIntersectLists[A](listA: List[A],
+                                           listB: List[A],
+                                           acc: List[A] = List.empty[A]): List[A] =
     listA match {
       case Nil => acc
       case head :: tail =>
         if (listB.contains(head)) {
-          intersectLists(tail, listB, acc :+ head)
+          factorialTimeIntersectLists(tail, listB, acc :+ head)
         } else {
-          intersectLists(tail, listB, acc)
+          factorialTimeIntersectLists(tail, listB, acc)
         }
     }
 }
--- src/test/scala/objektwerks/BigOTest.scala
+++ src/test/scala/objektwerks/BigOTest.scala
@@ -3,36 +3,38 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-import BigO._
+import scala.collection.mutable.ArrayBuffer
 
 class BigOTest extends AnyFunSuite with Matchers {
+  import BigO._
+
   test("constant time - O(1)") {
-    constantTime((1 to 10).toArray[Int])
+    constantTimeSearch((1 to 10).toArray[Int], 3) shouldBe 4
   }
 
   test("linear time - O(n)") {
-    linearTime((1 to 10).toArray[Int])
+    linearTimeSum( (0 to 9).toArray[Int] ) shouldBe 45
   }
 
   test("binary search - O(log N)") {
-    val values = List( Value(10), Value(9), Value(8), Value(7), Value(6), Value(5), Value(4), Value(3), Value(3), Value(2), Value(1) )
-    values.sorted
-    values.sorted.search(Value(3))
+    val buffer = ArrayBuffer[Value]()
+    for ( i <- 0 to 9 ) buffer.addOne( Value(i) )
+    binarySearch(buffer, Value(3)) shouldBe 3
   }
 
   test("quadratic time - O(n^2)") {
-    quadraticTime()
+    quadraticTimeMatrix().length shouldBe 3
   }
 
   test("exponential time - O(2^N)") {
-    fibonacci(39)
+    exponentialTimeFibonacci(39) shouldBe 63245986
   }
 
   test("factorial time - O(N!)") {
     val listA = (1 to 10).toList
     val listB = (5 to 15).toList
-    val intersectListsResult = intersectLists(listA, listB)
+    val intersectListsResult = factorialTimeIntersectLists(listA, listB)
     val intersectSdkResult = listA intersect listB
-    intersectListsResult == intersectSdkResult
+    intersectListsResult shouldEqual intersectSdkResult
   }
 }
