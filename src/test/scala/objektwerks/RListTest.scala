aab08d8c56d1bcef15da83c78443fc8375285e5c
objektwerks <objektwerks@runbox.com>
2020-12-07 08:24:01 -0500

    added iterable test

--- src/test/scala/objektwerks/RListTest.scala
+++ src/test/scala/objektwerks/RListTest.scala
@@ -13,4 +13,12 @@ class RListTest extends AnyFunSuite with Matchers {
     cons(2) shouldBe 3
     cons.reverse shouldBe 3 :: 2 :: 1 :: RNil
   }
+
+  test("iterable") {
+    val cons = RList.from(1 to 100)
+    cons.isEmpty shouldBe false
+    cons.length shouldBe 100
+    cons(50) shouldBe 50
+    cons.reverse shouldBe cons.reverse
+  }
 }
