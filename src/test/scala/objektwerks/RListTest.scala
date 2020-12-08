c1012d4c8739bedeeec985d736451704b86c25db
objektwerks <objektwerks@runbox.com>
2020-12-08 06:52:07 -0500

    renamed val in test for clarity

--- src/test/scala/objektwerks/RListTest.scala
+++ src/test/scala/objektwerks/RListTest.scala
@@ -6,19 +6,19 @@ import org.scalatest.matchers.should.Matchers
 import RList._
 
 class RListTest extends AnyFunSuite with Matchers {
-  test("list") {
-    val cons = 1 :: 2 :: 3 :: RNil
-    cons.isEmpty shouldBe false
-    cons.length shouldBe 3
-    cons(2) shouldBe 3
-    cons.reverse shouldBe 3 :: 2 :: 1 :: RNil
-    (cons ++ (4 :: RNil)) shouldBe 1 :: 2 :: 3 :: 4 :: RNil
-    (cons -= 2) shouldBe 1 :: 2 :: RNil
+  test("rlist") {
+    val rlist = 1 :: 2 :: 3 :: RNil
+    rlist.isEmpty shouldBe false
+    rlist.length shouldBe 3
+    rlist(2) shouldBe 3
+    rlist.reverse shouldBe 3 :: 2 :: 1 :: RNil
+    (rlist ++ (4 :: RNil)) shouldBe 1 :: 2 :: 3 :: 4 :: RNil
+    (rlist -= 2) shouldBe 1 :: 2 :: RNil
   }
 
   test("iterable") {
-    val cons = RList.iterable(1 to 1000)
-    cons.length shouldBe 1000
-    cons(500) shouldBe 500
+    val rlist = RList.iterable(1 to 1000)
+    rlist.length shouldBe 1000
+    rlist(500) shouldBe 500
   }
 }
