f592f5cca8effb2b6416365f87c1bb8118e84059
objektwerks <objektwerks@runbox.com>
2020-12-14 07:29:40 -0500

    implemented leaf count and collect leaves methods, to include test

--- src/main/scala/objektwerks/Trees.scala
+++ src/main/scala/objektwerks/Trees.scala
@@ -1,11 +1,16 @@
 package objektwerks
 
 object Trees {
+  import scala.annotation.tailrec
+
   sealed abstract class BTree[+T] {
     def value: T
     def left: BTree[T]
     def right: BTree[T]
     def isEmpty: Boolean
+    def isLeaf: Boolean
+    def collectLeaves: List[BTree[T]]
+    def leafCount: Int
   }
 
   case object BEnd extends BTree[Nothing] {
@@ -13,11 +18,29 @@ object Trees {
     override def left: BTree[Nothing] = throw new NoSuchElementException
     override def right: BTree[Nothing] = throw new NoSuchElementException
     override def isEmpty: Boolean = true
+    override def isLeaf: Boolean = false
+    override def collectLeaves: List[BTree[Nothing]] = List()
+    override def leafCount: Int = 0
   }
 
   case class BNode[+T](override val value: T,
-                   override val left: BTree[T],
-                   override val right: BTree[T]) extends BTree[T] {
+                       override val left: BTree[T],
+                       override val right: BTree[T]) extends BTree[T] {
     override def isEmpty: Boolean = false
+    override def isLeaf: Boolean = left.isEmpty && right.isEmpty
+    override def collectLeaves: List[BTree[T]] = {
+      @tailrec
+      def loop(todos: List[BTree[T]], leaves: List[BTree[T]]): List[BTree[T]] = {
+        if (todos.isEmpty) leaves
+        else if (todos.head.isEmpty) loop(todos.tail, leaves)
+        else if (todos.head.isLeaf) loop(todos.tail, todos.head :: leaves)
+        else {
+          val node = todos.head
+          loop(node.left :: node.right :: todos.tail, leaves)
+        }
+      }
+      loop(List(this), List())
+    }
+    override def leafCount: Int = collectLeaves.length
   }
 }
---
+++ src/test/scala/objektwerks/TreesTest.scala
@@ -0,0 +1,30 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+import Trees._
+
+class TreesTest extends AnyFunSuite with Matchers {
+  val tree = BNode(1,
+    BNode(2,
+      BNode( 3, BEnd, BEnd),
+      BNode(4,
+        BEnd,
+        BNode(5, BEnd, BEnd),
+      )
+    ),
+    BNode(6,
+      BNode(7, BEnd, BEnd),
+      BNode(8, BEnd, BEnd)
+    )
+  )
+
+  test("leaf count") {
+    tree.leafCount shouldBe 4
+  }
+
+  test("collect leaves") {
+    tree.collectLeaves.map(_.value) shouldBe List(8, 7, 5, 3)
+  }
+}
