1dc2776aa2da2ebd2af7189b2a5ed3baa0b6a142
objektwerks <objektwerks@runbox.com>
2020-12-19 08:10:21 -0500

    removed comment

--- src/main/scala/objektwerks/SierpinskiTrianglesApp.scala
+++ src/main/scala/objektwerks/SierpinskiTrianglesApp.scala
@@ -12,7 +12,7 @@ object SierpinskiTrianglesApp {
     def loop(currentLevel: Int, currentTriangle: List[String]): List[String] = {
       if (currentLevel >= n) currentTriangle
       else {
-        val spaces = " " * (1 << currentLevel) // 2 ^ (n-1) spaces
+        val spaces = " " * (1 << currentLevel)
         val topTriangle = currentTriangle.map(spaces + _ + spaces)
         val bottomTriangles = currentTriangle.map(row => row + " " + row)
         loop(currentLevel + 1, topTriangle ++ bottomTriangles)
