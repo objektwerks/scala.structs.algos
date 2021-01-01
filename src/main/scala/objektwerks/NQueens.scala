f6d69cf554d4a5dd8aee892abcb12e80ec6ca03f
objektwerks <objektwerks@runbox.com>
2021-01-01 12:01:49 -0500

    extracted nqueens into algos

--- src/main/scala/objektwerks/NQueens.scala
+++ src/main/scala/objektwerks/NQueens.scala
@@ -1,62 +1,8 @@
 package objektwerks
 
-object Algos {
-  import scala.annotation.tailrec
-  import scala.util.Try
+import scala.annotation.tailrec
 
-  @tailrec
-  def sum(xs: List[Int], acc: Int = 0): Int = xs match {
-    case Nil => acc
-    case head :: tail => sum(tail, acc + head)
-  }
-
-  @tailrec
-  def product(xs: List[Int], acc: Int = 1): Int = xs match {
-    case Nil => acc
-    case head :: tail => product(tail, acc * head)
-  }
-
-  @tailrec
-  def reverse[A](list: List[A], acc: List[A] = List.empty[A]): List[A] = list match {
-    case Nil => acc
-    case head :: tail => reverse(tail, head :: acc)
-  }
-
-  def findNthElementFromRight[A](list: List[A], nthElement: Int): Option[A] = {
-    @tailrec
-    def reverse(list: List[A], acc: List[A] = List.empty[A]): List[A] = list match {
-      case Nil => acc
-      case head :: tail => reverse(tail, head :: acc)
-    }
-    Try { reverse(list)(nthElement - 1) }.toOption
-  }
-
-  @tailrec
-  def factorial(n: Int, acc: Int = 1): Int = n match {
-    case i if i < 1 => acc
-    case _ => factorial(n - 1, acc * n)
-  }
-
-  def fibonacci(n: Long): BigInt = {
-    @tailrec
-    def loop(n: Long, a: Long, b: Long): BigInt = n match {
-      case 0 => a
-      case _ => loop(n - 1, b, a + b)
-    }
-    loop(n, 0, 1)
-  }
-
-  @tailrec
-  def intersectLists[A](listA: List[A],
-                        listB: List[A],
-                        acc: List[A] = List.empty[A]): List[A] =
-    listA match {
-      case Nil => acc
-      case head :: tail =>
-        if (listB.contains(head)) intersectLists(tail, listB, acc :+ head)
-        else intersectLists(tail, listB, acc)
-    }
-
+object NQueens {
   def nQueens(n: Int = 8): List[String] = {
     def conflict(position: Int, queens: List[Int]): Boolean = {
       def conflictOneQueen(position: Int, queen: Int, index: Int): Boolean =
