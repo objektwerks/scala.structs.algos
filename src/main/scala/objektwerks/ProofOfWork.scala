a25e3d0c4d1fa93f3f94305aaab0a21840e3724c
objektwerks <objektwerks@runbox.com>
2021-01-25 14:20:29 -0500

    refactored ProofOfWork

--- src/main/scala/objektwerks/ProofOfWork.scala
+++ src/main/scala/objektwerks/ProofOfWork.scala
@@ -5,10 +5,10 @@ final case class ProofOfWork(value: Long) extends Entity
 object ProofOfWork {
   import scala.annotation.tailrec
 
-  def calculate(lastHash: String): ProofOfWork = {
+  def solve(lastHash: String): ProofOfWork = {
     @tailrec
     def loop(lastHash: String, proof: Long): ProofOfWork = {
-      if ( isProofValid(lastHash, proof) )
+      if ( isValid(lastHash, proof) )
         ProofOfWork( proof )
       else
         loop(lastHash, proof + 1)
@@ -16,7 +16,7 @@ object ProofOfWork {
     loop(lastHash, 0)
   }
 
-  def isProofValid(lastHash: String, proof: Long): Boolean = {
+  private def isValid(lastHash: String, proof: Long): Boolean = {
     val guess = lastHash ++ proof.toString
     val guessHash = Hash.sha3256(guess)
     (guessHash.value take 4) == "0000"
