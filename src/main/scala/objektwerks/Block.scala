9a7bf0f6ec40e2d95e3a3d467c60503b963cfa38
objektwerks <objektwerks@runbox.com>
2021-01-25 12:58:06 -0500

    enhanced Entity

--- src/main/scala/objektwerks/Block.scala
+++ src/main/scala/objektwerks/Block.scala
@@ -1,17 +1,29 @@
 package objektwerks
 
-sealed trait Entity extends Product with Serializable
+import objektwerks.Entity._
 
-final case class Block[T](timestamp: Long,
+sealed trait Entity extends Product with Serializable
+object Entity {
+  import java.sql.Timestamp
+  import java.time.Instant
+
+  def timeStamp: Long = Timestamp.from(Instant.now).getTime
+
+  def hash(value: String): String = Hash.sha3256(value)
+
+  def proof(hash: String): Long = ProofOfWork.proof(hash)
+}
+
+final case class Block[T](timestamp: Long = timeStamp,
                           hash: String,
                           previousHash: String,
                           proof: Long,
                           data: List[Transaction[T]]) extends Entity
 
-final case class Transaction[T](timestamp: Long,
+final case class Transaction[T](timestamp: Long = timeStamp,
                                 hash: String,
                                 data: T) extends Entity
 
-final case class Chain[T](timestamp: Long,
+final case class Chain[T](timestamp: Long = timeStamp,
                           hash: String,
                           blocks: List[Block[T]]) extends Entity
