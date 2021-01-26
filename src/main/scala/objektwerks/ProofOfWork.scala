1891f20f7e4effb49c99388fd8598f35aa4bea40
objektwerks <objektwerks@runbox.com>
2021-01-26 05:27:33 -0500

    made ProofOfWork difficulty configurable by calling client, with a 4 default

--- src/main/scala/objektwerks/ProofOfWork.scala
+++ src/main/scala/objektwerks/ProofOfWork.scala
@@ -5,10 +5,10 @@ object ProofOfWork {
 
   import scala.annotation.tailrec
 
-  def solve(lastHash: String): ProofOfWork = {
+  def solve(lastHash: String, difficulty: Int = 4): ProofOfWork = {
     @tailrec
     def loop(lastHash: String, proof: Long): ProofOfWork = {
-      if ( isValid(lastHash, proof) )
+      if ( isValid(lastHash, proof, difficulty) )
         proof
       else
         loop(lastHash, proof + 1)
@@ -16,9 +16,9 @@ object ProofOfWork {
     loop(lastHash, 0)
   }
 
-  private def isValid(lastHash: String, proof: Long): Boolean = {
+  private def isValid(lastHash: String, proof: Long, difficulty: Int): Boolean = {
     val guess = lastHash ++ proof.toString
     val guessHash = Hash.sha3256(guess)
-    (guessHash take 4) == "0000"
+    ( guessHash take difficulty ) == ( "0" * difficulty )
   }
 }
