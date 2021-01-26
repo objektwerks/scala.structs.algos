86b23b141bbeeeec6514e1976cf8ed2249f36580
objektwerks <objektwerks@runbox.com>
2021-01-26 09:48:50 -0500

    enhanced BlockChainTest

--- src/test/scala/objektwerks/BlockBlockChainTest.scala
+++ src/test/scala/objektwerks/BlockBlockChainTest.scala
@@ -8,18 +8,13 @@ class BlockBlockChainTest extends AnyFunSuite with Matchers {
     val blockChain = new BlockChain[String]( genesisBlock = Block[String](value = "0") )
     blockChain.count shouldBe 1
     for (i <- 1 until 10) {
-      testBlock(blockChain, i.toString)
+      val block = Block[String](blockChain = blockChain, value = i.toString)
+      blockChain.add(block) shouldBe true
+      blockChain.get(block.hash) shouldBe Some(block)
+      blockChain.last.block.previousHash shouldBe block.previousHash
+      blockChain.last.hash shouldBe block.hash
+      blockChain.last.block shouldBe block
       blockChain.count shouldBe i + 1
     }
-  }
-
-  private def testBlock(blockChain: BlockChain[String], value: String): Unit = {
-    val block = Block[String](blockChain = blockChain, value = value)
-    blockChain.add(block) shouldBe true
-    blockChain.get(block.hash) shouldBe Some(block)
-    blockChain.last.block.previousHash shouldBe block.previousHash
-    blockChain.last.hash shouldBe block.hash
-    blockChain.last.block shouldBe block
-    ()
   }
 }
