d4773a9b06512c848a36b1ca95c33a2658e8617a
objektwerks <objektwerks@runbox.com>
2021-01-27 08:40:52 -0500

    added ProofOfWorkTest

---
+++ src/test/scala/objektwerks/ProofOfWorkTest.scala
@@ -0,0 +1,12 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class ProofOfWorkTest extends AnyFunSuite with Matchers {
+  test("proof of work") {
+    val text = "Dogfishhead 60' IPA is the best IPA in the world!"
+    val hash = Hash.sha3256(text)
+    val proofOfWork = ProofOfWork.mine(hash, 4)
+  }
+}
