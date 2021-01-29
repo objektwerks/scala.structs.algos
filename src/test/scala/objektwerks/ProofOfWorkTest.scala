6411ac20db2cfe1ee3ac98e2312b4a238104fdcb
objektwerks <objektwerks@runbox.com>
2021-01-29 11:03:08 -0500

    enhanced ProofOfWorkTest

--- src/test/scala/objektwerks/ProofOfWorkTest.scala
+++ src/test/scala/objektwerks/ProofOfWorkTest.scala
@@ -6,11 +6,9 @@ import org.scalatest.matchers.should.Matchers
 class ProofOfWorkTest extends AnyFunSuite with Matchers {
   test("proof of work") {
     val text = "Dogfishhead 60' IPA is the best IPA in the world!"
-    val hash = Hash.sha3256(text)
-    val proofOfWork = ProofOfWork.mine(Hash.sha3256(text), 4)
+    val proofOfWork = ProofOfWork.mine( Hash.sha3256(text), 4 )
 
-    proofOfWork shouldBe ProofOfWork.mine(Hash.sha3256(text), 4)
-    proofOfWork should not be ProofOfWork.mine(Hash.sha3256(text + "!!"), 4)
-    ProofOfWork.mine(hash, 4) > 0 shouldBe true
+    proofOfWork shouldBe ProofOfWork.mine( Hash.sha3256(text), 4 )
+    proofOfWork should not be ProofOfWork.mine( Hash.sha3256(text + "!!"), 4 )
   }
 }
