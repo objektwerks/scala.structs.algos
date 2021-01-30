0067a3b9cbb7864bfe33f021b29805b94d2458ef
objektwerks <objektwerks@runbox.com>
2021-01-30 09:03:23 -0500

    added keypair generator to AsymmetricCrypto

--- src/main/scala/objektwerks/AsymmetricCrypto.scala
+++ src/main/scala/objektwerks/AsymmetricCrypto.scala
@@ -1,5 +1,15 @@
 package objektwerks
 
 object AsymmetricCrypto {
+  import java.security.{KeyPairGenerator, PrivateKey, PublicKey}
 
+  import scala.util.Try
+
+  def generateKeyPair: Either[Throwable, (PrivateKey, PublicKey)] =
+    Try {
+      val keyPairGenerator = KeyPairGenerator.getInstance("RSA")
+      keyPairGenerator.initialize(1024)
+      val keyPair = keyPairGenerator.generateKeyPair
+      (keyPair.getPrivate, keyPair.getPublic)
+    }.toEither
 }
