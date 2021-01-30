819fe45407c3d81ee897fc14390d523e2d31975f
objektwerks <objektwerks@runbox.com>
2021-01-30 09:15:18 -0500

    added encrypt and decrypt methods to AsymmetricCrypto

--- src/main/scala/objektwerks/AsymmetricCrypto.scala
+++ src/main/scala/objektwerks/AsymmetricCrypto.scala
@@ -2,8 +2,28 @@ package objektwerks
 
 object AsymmetricCrypto {
   import java.security.{KeyPairGenerator, PrivateKey, PublicKey}
+  import java.util.Base64
+  import javax.crypto.Cipher
 
   import scala.util.Try
+
+
+  def encrypt(text: String, privateKey: PrivateKey): Either[Throwable, String] =
+    Try {
+      val cipher = Cipher.getInstance("RSA")
+      cipher.init(Cipher.ENCRYPT_MODE, privateKey)
+      val encryptedBytes = cipher.doFinal( text.getBytes("UTF-8") )
+      Base64.getEncoder.encodeToString(encryptedBytes)
+    }.toEither
+
+  def decrypt(encryptedText: String, publicKey: PublicKey): Either[Throwable, String] =
+    Try {
+      val cipher = Cipher.getInstance("RSA")
+      cipher.init(Cipher.DECRYPT_MODE, publicKey)
+      val decodedBytes = Base64.getDecoder.decode(encryptedText)
+      val decryptedBytes = cipher.doFinal(decodedBytes)
+      new String(decryptedBytes)
+    }.toEither
 
   def generateKeyPair: Either[Throwable, (PrivateKey, PublicKey)] =
     Try {
