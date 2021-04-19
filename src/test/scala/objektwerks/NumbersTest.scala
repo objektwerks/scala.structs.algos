804b039f71f745cd5ceba941aa82341eb4ef564a
objektwerks <objektwerks@runbox.com>
2021-04-19 08:14:38 -0400

    added reverse integer algo test

--- src/test/scala/objektwerks/NumbersTest.scala
+++ src/test/scala/objektwerks/NumbersTest.scala
@@ -48,4 +48,11 @@ class NumbersTest extends AnyFunSuite with Matchers {
     largestNumber(List(11, 1)) shouldBe 111
     largestNumber(List(1, 2, 3, 4, 5, 6)) shouldBe 654321
   }
+
+  test("reverse integer") {
+    reverseInteger(0) shouldBe 0
+    reverseInteger(3) shouldBe 3
+    reverseInteger(36) shouldBe 63
+
+  }
 }
