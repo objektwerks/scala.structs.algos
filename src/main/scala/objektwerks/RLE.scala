d97b1a12bc9102c77c4d4d3bb0c7a63ecad94d95
objektwerks <objektwerks@runbox.com>
2021-08-19 07:44:16 -0400

    added rle

---
+++ src/main/scala/objektwerks/RLE.scala
@@ -0,0 +1,41 @@
+package objektwerks
+
+/**
+ * Encoding for single letter occurences includes a 1 so that decode works consistently.
+ * Decoding won't work for char counts beyond 9.
+ */
+object RLE {
+  final case class Encoding(char: Char, count: Int) extends Product with Serializable
+
+  def encode(value: String): String = {
+    def group(chars: List[Char]): List[List[Char]] = {
+      if (chars.isEmpty) List(List())
+      else {
+        val (grouped, next) = chars span { char => char == chars.head }
+        if (next == Nil) List(grouped)
+        else grouped :: group(next)
+      }
+    }
+    val valueAsChars = value.toCharArray.toList
+    val encodings = group(valueAsChars) map { chars => Encoding(chars.head, chars.length) }
+    val encodedValues = encodings map { group =>
+      group.char.toString + group.count.toString
+    }
+    encodedValues.mkString
+  }
+
+  def decode(value: String): String = {
+    var decoded: String = ""
+    var count: Int = 0
+    val result = new StringBuilder()
+    value.toCharArray.toList.foreach { char =>
+      if ( char.isDigit ) {
+        count = char.asDigit
+        result.append( decoded * count )
+      } else {
+        decoded = char.toString
+      }
+    }
+    result.mkString
+  }
+}
