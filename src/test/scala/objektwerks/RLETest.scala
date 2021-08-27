1457e72e0a0e8a715d2f294ff5943a5d4430e647
objektwerks <objektwerks@runbox.com>
2021-08-27 19:12:21 -0400

    enhanced rle and test

--- src/test/scala/objektwerks/RLETest.scala
+++ src/test/scala/objektwerks/RLETest.scala
@@ -5,16 +5,18 @@ import org.scalatest.matchers.should.Matchers
 
 class RLETest extends AnyFunSuite with Matchers {
   test("encode") {
-    println( s" *** Run Length Encoding: ${ RLE.encode("aaaabbcccaeeeee") }" )
+    println( s"*** RLE of aaaabbcccaeeeee : ${ RLE.encode("aaaabbcccaeeeee") }" )
     RLE.encode("aaaabbcccaeeeee") shouldBe "a4b2c3a1e5"
     RLE.encode("") shouldBe ""
+    println( s"*** RLE of aaaaaaaaaaaaaaaaa : ${ RLE.encode("aaaaaaaaaaaaaaaaa") }" )
+    RLE.encode("aaaaaaaaaaaaaaaaa") shouldBe "a17"
   }
 
   test("decode") {
-    println( s" *** Run Length Decoding: ${ RLE.decode("a4b2c3a1e5") }" )
+    println( s"*** RLE of a4b2c3a1e5 : ${ RLE.decode("a4b2c3a1e5") }" )
     RLE.decode("a4b2c3a1e5") shouldBe "aaaabbcccaeeeee"
     RLE.decode("") shouldBe ""
-    println( s"*** Run Length Decoding: ${ RLE.decodex("a17") }" )
+    println( s"*** RRLE of a17 : ${ RLE.decodex("a17") }" )
     RLE.decodex("a17").length shouldBe 17
   }
 }
