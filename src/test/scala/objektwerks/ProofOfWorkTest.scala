25bb2de15fec0b52d64971dad91ee713e343db17
objektwerks <objektwerks@runbox.com>
2021-12-02 07:18:02 -0500

    reformatted to braceless style

--- src/test/scala/objektwerks/ProofOfWorkTest.scala
+++ src/test/scala/objektwerks/ProofOfWorkTest.scala
@@ -3,12 +3,10 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class ProofOfWorkTest extends AnyFunSuite with Matchers {
+class ProofOfWorkTest extends AnyFunSuite with Matchers:
   test("proof of work") {
     val text = "Dogfishhead 60' IPA is the best IPA in the world!"
     val proofOfWork = ProofOfWork.mine( Hash.sha3256(text), 4 )
-
     proofOfWork shouldBe ProofOfWork.mine( Hash.sha3256(text), 4 )
     proofOfWork should not be ProofOfWork.mine( Hash.sha3256(text + "!!"), 4 )
   }
-}
