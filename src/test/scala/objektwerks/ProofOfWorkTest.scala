60b5f3a02052bcb845136243205b5ae620168b38
objektwerks <objektwerks@runbox.com>
2021-12-02 07:20:59 -0500

    corrected redundant arg errors

--- src/test/scala/objektwerks/ProofOfWorkTest.scala
+++ src/test/scala/objektwerks/ProofOfWorkTest.scala
@@ -6,7 +6,7 @@ import org.scalatest.matchers.should.Matchers
 class ProofOfWorkTest extends AnyFunSuite with Matchers:
   test("proof of work") {
     val text = "Dogfishhead 60' IPA is the best IPA in the world!"
-    val proofOfWork = ProofOfWork.mine( Hash.sha3256(text), 4 )
-    proofOfWork shouldBe ProofOfWork.mine( Hash.sha3256(text), 4 )
-    proofOfWork should not be ProofOfWork.mine( Hash.sha3256(text + "!!"), 4 )
+    val proofOfWork = ProofOfWork.mine( Hash.sha3256(text))
+    proofOfWork shouldBe ProofOfWork.mine( Hash.sha3256(text))
+    proofOfWork should not be ProofOfWork.mine( Hash.sha3256(text + "!!"))
   }
