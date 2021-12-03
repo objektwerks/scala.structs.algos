697414b508a42180a1276809810152c4c616d0a1
objektwerks <objektwerks@runbox.com>
2021-12-03 02:41:23 -0500

    refactored to use braceless style

--- src/main/scala/objektwerks/Graphs.scala
+++ src/main/scala/objektwerks/Graphs.scala
@@ -1,6 +1,6 @@
 package objektwerks
 
-object Graphs {
+object Graphs:
   import scala.annotation.tailrec
 
   type Graph[T] = Map[T, Set[T]]
@@ -14,19 +14,17 @@ object Graphs {
   )
 
   // Number of nodes adjacent to node - or node's set count.
-  def outDegree[T](graph: Graph[T], node: T): Int = {
+  def outDegree[T](graph: Graph[T], node: T): Int =
     if (graph.contains(node)) graph(node).size
     else 0
-  }
 
   // Number of nodes connected to node - or node occurences in all sets.
-  def inDegree[T](graph: Graph[T], node: T): Int = {
+  def inDegree[T](graph: Graph[T], node: T): Int =
     graph.values.count(_.contains(node))
-  }
 
-  def isPath[T](graph: Graph[T], start: T, end: T): Boolean = {
+  def isPath[T](graph: Graph[T], start: T, end: T): Boolean =
     @tailrec
-    def loop(list: List[T], visited: Set[T]): Boolean = {
+    def loop(list: List[T], visited: Set[T]): Boolean =
       if (list.isEmpty) false
       else {
         val node = list.head
@@ -34,13 +32,11 @@ object Graphs {
         else if (visited.contains(node)) loop(list.tail, visited)
         else loop(list.tail ++ graph(node), visited + node)
       }
-    }
     loop(List(start), Set())
-  }
 
-  def findPath[T](graph: Graph[T], start: T, end: T): List[T] = {
+  def findPath[T](graph: Graph[T], start: T, end: T): List[T] =
     @tailrec
-    def loop(list: List[(T, List[T])], visited: Set[T]): List[T] = {
+    def loop(list: List[(T, List[T])], visited: Set[T]): List[T] =
       if (list.isEmpty) List()
       else {
         val (node, path) = list.head
@@ -52,26 +48,24 @@ object Graphs {
           loop(list.tail ++ tuples, visited + node)
         }
       }
-    }
     loop(
       list = graph(start).map(n => (n, n :: List(start))).toList,
       visited = Set(start)
     )
-  }
 
   def findCycle[T](graph: Graph[T], node: T): List[T] = findPath(graph, node, node)
 
   // Bidirectional graph.
-  def toUndirected[T](graph: Graph[T]): Graph[T] = {
-    def addEdge(graph: Graph[T], from: T, to: T): Graph[T] = {
+  def toUndirected[T](graph: Graph[T]): Graph[T] =
+    def addEdge(graph: Graph[T], from: T, to: T): Graph[T] =
       if (!graph.contains(from)) graph + (from -> Set(to))
       else {
         val neighbors = graph(from)
         graph + (from -> (neighbors + to))
       }
-    }
+
     @tailrec
-    def addOpposingEdges(list: Set[T], acc: Graph[T]): Graph[T] = {
+    def addOpposingEdges(list: Set[T], acc: Graph[T]): Graph[T] =
       if (list.isEmpty) acc
       else {
         val node = list.head
@@ -79,13 +73,11 @@ object Graphs {
         val newGraph = neighbors.foldLeft(acc)((graph, neighbor) => addEdge(graph, neighbor, node))
         addOpposingEdges(list.tail, newGraph)
       }
-    }
     addOpposingEdges(graph.keySet, graph)
-  }
 
-  def color[T](undirectedGraph: Graph[T]): Map[T, Int] = {
+  def color[T](undirectedGraph: Graph[T]): Map[T, Int] =
     @tailrec
-    def loop(list: List[T], current: Int, acc: Map[T, Int]): Map[T, Int] = {
+    def loop(list: List[T], current: Int, acc: Map[T, Int]): Map[T, Int] =
       if (list.isEmpty) acc
       else {
         val node = list.head
@@ -100,10 +92,7 @@ object Graphs {
           loop(list.tail, current + 1, acc ++ newColoredNodes)
         }
       }
-    }
     val orderedNodes = undirectedGraph.keySet.toList.sortWith((a, b) =>
       outDegree(undirectedGraph, a) > outDegree(undirectedGraph, b)
     )
     loop(orderedNodes, 0, Map())
-  }
-}
