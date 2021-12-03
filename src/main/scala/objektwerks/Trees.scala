f742a505cc8952553d2c12fafdb39e6a3f13cf5f
objektwerks <objektwerks@runbox.com>
2021-12-03 02:43:08 -0500

    refactored to use braceless style

--- src/main/scala/objektwerks/Trees.scala
+++ src/main/scala/objektwerks/Trees.scala
@@ -1,10 +1,10 @@
 package objektwerks
 
-object Trees {
+object Trees:
   import scala.annotation.tailrec
   import scala.collection.immutable.Queue
 
-  sealed abstract class BTree[+T] {
+  sealed abstract class BTree[+T]:
     def value: T
     def left: BTree[T]
     def right: BTree[T]
@@ -18,9 +18,8 @@ object Trees {
     def sameShapeAs[S >: T](other: BTree[S]): Boolean
     def isSymmetrical: Boolean
     def toList: List[T]
-  }
 
-  case object BEnd extends BTree[Nothing] {
+  case object BEnd extends BTree[Nothing]:
     override def value: Nothing = throw new NoSuchElementException
     override def left: BTree[Nothing] = throw new NoSuchElementException
     override def right: BTree[Nothing] = throw new NoSuchElementException
@@ -34,18 +33,17 @@ object Trees {
     override def sameShapeAs[S >: Nothing](other: BTree[S]): Boolean = false
     override def isSymmetrical: Boolean = true
     override def toList: List[Nothing] = List()
-  }
 
   case class BNode[+T](override val value: T,
                        override val left: BTree[T],
-                       override val right: BTree[T]) extends BTree[T] {
+                       override val right: BTree[T]) extends BTree[T]:
     override def isEmpty: Boolean = false
     override def isLeaf: Boolean = left.isEmpty && right.isEmpty
     override def leafCount: Int = collectLeaves.length
     override val size: Int = 1 + left.size + right.size
-    override def collectLeaves: List[BTree[T]] = {
+    override def collectLeaves: List[BTree[T]] =
       @tailrec
-      def loop(todos: List[BTree[T]], leaves: List[BTree[T]]): List[BTree[T]] = {
+      def loop(todos: List[BTree[T]], leaves: List[BTree[T]]): List[BTree[T]] =
         if (todos.isEmpty) leaves
         else if (todos.head.isEmpty) loop(todos.tail, leaves)
         else if (todos.head.isLeaf) loop(todos.tail, todos.head :: leaves)
@@ -53,12 +51,11 @@ object Trees {
           val node = todos.head
           loop(node.left :: node.right :: todos.tail, leaves)
         }
-      }
       loop(List(this), List())
-    }
-    override def collectNodes(level: Int): List[BTree[T]] = {
+
+    override def collectNodes(level: Int): List[BTree[T]] =
       @tailrec
-      def loop(currentLevel: Int, currentNodes: List[BTree[T]]): List[BTree[T]] = {
+      def loop(currentLevel: Int, currentNodes: List[BTree[T]]): List[BTree[T]] =
         if (currentNodes.isEmpty) List()
         else if (currentLevel == level) currentNodes
         else {
@@ -68,12 +65,11 @@ object Trees {
           } yield child
           loop(currentLevel + 1, expandedNodes)
         }
-      }
       if (level < 0) List() else loop(0, List(this))
-    }
-    override def mirror: BTree[T] = {
+
+    override def mirror: BTree[T] =
       @tailrec
-      def loop(todos: List[BTree[T]], expanded: Set[BTree[T]], acc: List[BTree[T]]): BTree[T] = {
+      def loop(todos: List[BTree[T]], expanded: Set[BTree[T]], acc: List[BTree[T]]): BTree[T] =
         if (todos.isEmpty) acc.head
         else {
           val node = todos.head
@@ -88,12 +84,11 @@ object Trees {
             loop(todos.tail, expanded, newNode :: acc.drop(2))
           }
         }
-      }
       loop(List(this), Set(), List())
-    }
-    override def sameShapeAs[S >: T](other: BTree[S]): Boolean = {
+
+    override def sameShapeAs[S >: T](other: BTree[S]): Boolean =
       @tailrec
-      def loop(thisTree: List[BTree[S]], otherTree: List[BTree[S]]): Boolean = {
+      def loop(thisTree: List[BTree[S]], otherTree: List[BTree[S]]): Boolean =
         if (thisTree.isEmpty) otherTree.isEmpty
         else if (otherTree.isEmpty) thisTree.isEmpty
         else {
@@ -106,11 +101,11 @@ object Trees {
             thatNode.left :: thatNode.right :: otherTree.tail
           )
         }
-      }
       loop(List(this), List(other))
-    }
+
     override def isSymmetrical: Boolean = sameShapeAs(this.mirror)
-    override def toList: List[T] = {
+    
+    override def toList: List[T] =
       @tailrec
       def perLevelTailrec(level: List[BTree[T]], finalQueue: Queue[BTree[T]] = Queue()): List[T] =
         if (level.isEmpty) finalQueue.map(_.value).toList
@@ -119,6 +114,3 @@ object Trees {
           finalQueue ++ level
         )
       perLevelTailrec(List(this))
-    }
-  }
-}
