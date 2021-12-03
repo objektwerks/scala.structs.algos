812c4e4f44b8a4927fd8e0f3e37cc27ad32b16ec
objektwerks <objektwerks@runbox.com>
2021-12-03 01:51:30 -0500

    refactored main

--- src/main/scala/objektwerks/app/EvalApp.scala
+++ src/main/scala/objektwerks/app/EvalApp.scala
@@ -1,13 +1,11 @@
 package objektwerks.app
 
-object EvalApp {
+object EvalApp:
   import objektwerks.Eval._
 
-  def main(args: Array[String]): Unit = {
+  @main def runEval(): Unit =
     println(s"1 + 2 = ${eval("1 + 2")}")
     println(s"4 - 1 = ${eval("4 - 1")}")
     println(s"1 * 3 = ${eval("1 * 3")}")
     println(s"9 / 3 = ${eval("9 / 3")}")
     println(s"1 + 2 * 3 + 4 / 5 + 6 * 7 - 8 = ${eval("1 + 2 * 3 + 4 / 5 + 6 * 7 - 8")}")
-  }
-}
