8a8c0243ef1b7da86c20c2728a5e6d2b086cd870
objektwerks <objektwerks@runbox.com>
2021-12-03 02:42:01 -0500

    refactored to use braceless style

--- src/main/scala/objektwerks/ProofOfWork.scala
+++ src/main/scala/objektwerks/ProofOfWork.scala
@@ -1,22 +1,19 @@
 package objektwerks
 
-object ProofOfWork {
+object ProofOfWork:
   import scala.annotation.tailrec
 
   type ProofOfWork = Long
 
-  def mine(blockHash: String, difficulty: Int = 4): ProofOfWork = {
+  def mine(blockHash: String, difficulty: Int = 4): ProofOfWork =
     @tailrec
     def loop(hash: String, proof: Long): ProofOfWork =
       if ( isValid(hash, proof, difficulty) ) proof
       else loop(hash, proof + 1)
 
     loop(blockHash, 0L)
-  }
 
-  private def isValid(hash: String, proof: Long, difficulty: Int): Boolean = {
+  private def isValid(hash: String, proof: Long, difficulty: Int): Boolean =
     val guess = hash ++ proof.toString
     val guessHash = Hash.sha3256(guess)
     ( guessHash take difficulty ) == ( "0" * difficulty )
-  }
-}
