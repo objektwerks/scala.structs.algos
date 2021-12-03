1d19093c860b20110253ef67c0b060b9ec5f168f
objektwerks <objektwerks@runbox.com>
2021-12-03 02:41:36 -0500

    refactored to use braceless style

--- src/main/scala/objektwerks/Hash.scala
+++ src/main/scala/objektwerks/Hash.scala
@@ -1,18 +1,17 @@
 package objektwerks
 
-object Hash {
+object Hash:
   import java.nio.charset.StandardCharsets
   import java.security.MessageDigest
 
   type Hash = String
 
-  def sha3256(value: String): Hash = {
+  def sha3256(value: String): Hash =
     val digest = MessageDigest.getInstance("SHA3-256")
     val hashBytes = digest.digest(value.getBytes(StandardCharsets.UTF_8))
     bytesToHexString(hashBytes)
-  }
 
-  private def bytesToHexString(hashBytes: Array[Byte]): String = {
+  private def bytesToHexString(hashBytes: Array[Byte]): String =
     val hexStringBuilder = new StringBuilder(2 * hashBytes.length)
     for (i <- hashBytes.indices) {
       val hexString = Integer.toHexString(0xff & hashBytes(i))
@@ -20,5 +19,3 @@ object Hash {
       hexStringBuilder.append(hexString)
     }
     hexStringBuilder.toString
-  }
-}
