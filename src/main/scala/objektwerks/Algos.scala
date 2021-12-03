bced49bf08690f735dee6bea5f1f2ec26c2927bc
objektwerks <objektwerks@runbox.com>
2021-12-03 02:40:04 -0500

    refactored to use braceless style

--- src/main/scala/objektwerks/Algos.scala
+++ src/main/scala/objektwerks/Algos.scala
@@ -1,6 +1,6 @@
 package objektwerks
 
-object Algos {
+object Algos:
   import scala.annotation.tailrec
   import scala.util.Try
 
@@ -22,14 +22,13 @@ object Algos {
     case head :: tail => reverse(tail, head :: acc)
   }
 
-  def findNthElementFromRight[A](list: List[A], nthElement: Int): Option[A] = {
+  def findNthElementFromRight[A](list: List[A], nthElement: Int): Option[A] =
     @tailrec
     def reverse(list: List[A], acc: List[A] = List.empty[A]): List[A] = list match {
       case Nil => acc
       case head :: tail => reverse(tail, head :: acc)
     }
     Try { reverse(list)(nthElement - 1) }.toOption
-  }
 
   @tailrec
   def factorial(n: Int, acc: Int = 1): Int = n match {
@@ -37,14 +36,13 @@ object Algos {
     case _ => factorial(n - 1, acc * n)
   }
 
-  def fibonacci(n: Long): BigInt = {
+  def fibonacci(n: Long): BigInt =
     @tailrec
     def loop(n: Long, a: Long, b: Long): BigInt = n match {
       case 0 => a
       case _ => loop(n - 1, b, a + b)
     }
     loop(n, 0, 1)
-  }
 
   @tailrec
   def intersectLists[A](listA: List[A],
@@ -57,17 +55,17 @@ object Algos {
         else intersectLists(tail, listB, acc)
     }
 
-  def nQueens(n: Int = 8): List[String] = {
-    def conflict(position: Int, queens: List[Int]): Boolean = {
+  def nQueens(n: Int = 8): List[String] =
+    def conflict(position: Int, queens: List[Int]): Boolean =
       def conflictOneQueen(position: Int, queen: Int, index: Int): Boolean =
         queen == position || (index + 1) == (position - queen) || (index + 1) == (queen - position)
       queens.zipWithIndex.exists { pair =>
         val (queen, index) = pair
         conflictOneQueen(position, queen, index)
       }
-    }
+
     @tailrec
-    def loop(currentPosition: Int, currentQueens: List[Int], solutions: List[List[Int]]): List[List[Int]] = {
+    def loop(currentPosition: Int, currentQueens: List[Int], solutions: List[List[Int]]): List[List[Int]] =
       // I'm out of options
       if (currentPosition >= n && currentQueens.isEmpty) solutions
       else if (currentPosition >= n) {
@@ -84,8 +82,8 @@ object Algos {
         // try next queen on the next row, as this one is valid
         loop(0, currentPosition :: currentQueens, solutions)
       }
-    }
-    def prettyPrint(solution: List[Int]): String = {
+
+    def prettyPrint(solution: List[Int]): String =
       val topEdge = (1 to n).map(_ => "_").mkString(".", ".", ".") // ._._._._.
       val rows = solution.map { queen =>
         val cellsBefore = (0 until queen).map(_ => "_")
@@ -95,13 +93,11 @@ object Algos {
         beforeString + "x" + afterString
       }
       s"$topEdge\n${rows.mkString("\n")}"
-    }
     loop(0, List(), List()).map(prettyPrint)
-  }
 
-  def sierpinski(n: Int): String = {
+  def sierpinski(n: Int): String =
     @tailrec
-    def loop(currentLevel: Int, currentTriangle: List[String]): List[String] = {
+    def loop(currentLevel: Int, currentTriangle: List[String]): List[String] =
       if (currentLevel >= n) currentTriangle
       else {
         val spaces = " " * (1 << currentLevel) // 2 ^ (n - 1) spaces
@@ -109,23 +105,18 @@ object Algos {
         val bottomTriangles = currentTriangle.map(row => row + " " + row)
         loop(currentLevel + 1, topTriangle ++ bottomTriangles)
       }
-    }
     loop(0, List("*")).mkString("\n")
-  }
 
-  object Triangle {
+  object Triangle:
     trait Kind
     case object scalene extends Kind     // 0 sides equal
     case object isoceles extends Kind    // 2 sides equal
     case object equilateral extends Kind // 3 sides equal
-  }
 
-  case class Triangle(a: Int, b: Int, c: Int) {
+  case class Triangle(a: Int, b: Int, c: Int):
     import Triangle._
     def kind: Kind = (a, b, c) match {
       case (x, y, z) if x == y && y == z => equilateral
       case (x, y, z) if x == y || y == z || z == x => isoceles
       case _ => scalene
     }
-  }
-}
