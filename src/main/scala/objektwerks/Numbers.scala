e1f535f218c85d6511dff49f5e32892ac35fec01
objektwerks <objektwerks@runbox.com>
2021-12-03 03:01:27 -0500

    refactored to use braceless style

--- src/main/scala/objektwerks/Numbers.scala
+++ src/main/scala/objektwerks/Numbers.scala
@@ -8,19 +8,19 @@ object Numbers:
 
   def isPrime(n: Int): Boolean =
     @tailrec
-    def loop(current: Int): Boolean = {
+    def loop(current: Int): Boolean =
       if (current > sqrt(abs(n.toDouble))) true
       else n % current != 0 && loop(current + 1)
-    }
+
     if (n == -1 || n == 0 || n == 1) false else loop(2)
 
   def listFactors(n: Int): List[Int] =
     @tailrec
-    def loop(remaining: Int, currentDivisor: Int, acc: List[Int]): List[Int] = {
+    def loop(remaining: Int, currentDivisor: Int, acc: List[Int]): List[Int] =
       if (currentDivisor > sqrt(remaining.toDouble)) remaining :: acc
       else if (remaining % currentDivisor == 0) loop(remaining / currentDivisor, currentDivisor, currentDivisor :: acc)
       else loop(remaining, currentDivisor + 1, acc)
-    }
+
     if (n < 1) List.empty[Int] else loop(n, 2, List())
 
   def approximatePi(points: Int): Double =
@@ -33,15 +33,22 @@ object Numbers:
     pointInCircle * 4.0 / points
 
   def fractionToRecurringDecimals(numerator: Long, denominator: Long): String =
-    def fractionToDecimal(n: Long, d: Long): String = {
+    def fractionToDecimal(n: Long, d: Long): String =
       @tailrec
-      def findStart(digit: Long, digits: List[Long], rem: Long, remainders: List[Long], currentIndex: Int): Int = {
+      def findStart(digit: Long,
+                    digits: List[Long],
+                    rem: Long,
+                    remainders: List[Long],
+                    currentIndex: Int): Int =
         if (digits.isEmpty || remainders.isEmpty) -1
         else if (digit == digits.head && rem == remainders.head) currentIndex
         else findStart(digit, digits.tail, rem, remainders.tail, currentIndex + 1)
-      }
+
       @tailrec
-      def loop(num: Long, denominator: Long, digits: List[Long], remainders: List[Long]): String = {
+      def loop(num: Long,
+               denominator: Long,
+               digits: List[Long],
+               remainders: List[Long]): String =
         val quot = (num * 10) / denominator
         val rem = (num * 10) % denominator
         if (rem == 0) (digits :+ quot).mkString("")
@@ -53,7 +60,6 @@ object Numbers:
             s"${beforeRecurrence.mkString("")}(${recurrence.mkString("")})"
           }
         }
-      }
       if (n > 0 && d < 0) s"-${fractionToDecimal(n, -d)}"
       else if (n < 0 && d > 0) s"-${fractionToDecimal(-n, d)}"
       else {
@@ -62,7 +68,6 @@ object Numbers:
         if (remainder == 0) s"$quotient"
         else s"$quotient.${loop(remainder, d, List(), List())}"
       }
-    }
     fractionToDecimal(numerator, denominator)
 
   def largestNumber(numbers: List[Int]): Int =
@@ -85,6 +90,7 @@ object Numbers:
         if ((acc >= 0) != (tentativeResult >= 0)) 0
         else loop(remaining / 10, tentativeResult)
       }
+      
     if (number == Int.MinValue) 0
     else if (number >= 0) loop(number, 0)
     else -loop(-number, 0)
