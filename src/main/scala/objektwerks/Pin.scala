68667439e10505592b5e43f7f07b5dfc344ce919
objektwerks <objektwerks@runbox.com>
2022-04-04 13:12:27 -0400

    added Pin

---
+++ src/main/scala/objektwerks/Pin.scala
@@ -0,0 +1,23 @@
+package objektwerks
+
+import scala.util.Random
+
+object Pin:
+  private val specialChars = "~!@#$%^&*-+=<>?/:;".toList
+  private val random = new Random
+
+  private def newSpecialChar: Char = specialChars(random.nextInt(specialChars.length))
+
+    /**
+   * 26 letters + 10 numbers + 18 special characters = 54 combinations
+   * 7 alphanumeric char pin = 54^7 ( 1,338,925,209,984 )
+   */
+  def apply(): String =
+    Random.shuffle(
+      Random
+        .alphanumeric
+        .take(5)
+        .mkString
+        .prepended(newSpecialChar)
+        .appended(newSpecialChar)
+    ).mkString
