380fe14c4c0a00f09ce95f13e500f6cb3d0e87d4
objektwerks <objektwerks@runbox.com>
2023-01-30 05:37:30 -0500

    added sierpinski

--- src/main/scala/objektwerks/Algos.scala
+++ src/main/scala/objektwerks/Algos.scala
@@ -97,15 +97,3 @@ object Algos:
       s"$topEdge\n${rows.mkString("\n")}"
       
     loop(0, List(), List()).map(prettyPrint)
-
-  def sierpinski(n: Int): String =
-    @tailrec
-    def loop(currentLevel: Int, currentTriangle: List[String]): List[String] =
-      if currentLevel >= n then currentTriangle
-      else
-        val spaces = " " * (1 << currentLevel) // 2 ^ (n - 1) spaces
-        val topTriangle = currentTriangle.map(spaces + _ + spaces)
-        val bottomTriangles = currentTriangle.map(row => row + " " + row)
-        loop(currentLevel + 1, topTriangle ++ bottomTriangles)
-
-    loop(0, List("*")).mkString("\n")
---
+++ src/main/scala/objektwerks/Sierpinski.scala
@@ -0,0 +1,16 @@
+package objektwerks
+
+import scala.annotation.tailrec
+
+class Sierpinski:
+  def sierpinski(n: Int): String =
+    @tailrec
+    def loop(currentLevel: Int, currentTriangle: List[String]): List[String] =
+      if currentLevel >= n then currentTriangle
+      else
+        val spaces = " " * (1 << currentLevel) // 2 ^ (n - 1) spaces
+        val topTriangle = currentTriangle.map(spaces + _ + spaces)
+        val bottomTriangles = currentTriangle.map(row => row + " " + row)
+        loop(currentLevel + 1, topTriangle ++ bottomTriangles)
+
+    loop(0, List("*")).mkString("\n")
