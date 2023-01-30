8c515a7af2f565eb845be61576b7016197c216fd
objektwerks <objektwerks@runbox.com>
2023-01-30 05:33:29 -0500

    added triangle

---
+++ src/main/scala/objektwerks/Triangle.scala
@@ -0,0 +1,15 @@
+package objektwerks
+
+object Triangle:
+  trait Kind
+  case object scalene extends Kind     // 0 sides equal
+  case object isoceles extends Kind    // 2 sides equal
+  case object equilateral extends Kind // 3 sides equal
+
+case class Triangle(a: Int, b: Int, c: Int):
+  import Triangle._
+  def kind: Kind =
+    (a, b, c) match
+      case (x, y, z) if x == y && y == z => equilateral
+      case (x, y, z) if x == y || y == z || z == x => isoceles
+      case _ => scalene
