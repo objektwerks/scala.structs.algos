0d70f606c9a1740e3d43a434588e435e60d61bf8
objektwerks <objektwerks@runbox.com>
2023-01-30 06:11:59 -0500

    edited triangle

--- src/main/scala/objektwerks/Triangle.scala
+++ src/main/scala/objektwerks/Triangle.scala
@@ -7,7 +7,8 @@ object Triangle:
   case object equilateral extends Kind // 3 sides equal
 
 case class Triangle(a: Int, b: Int, c: Int):
-  import Triangle._
+  import Triangle.*
+  
   def kind: Kind =
     (a, b, c) match
       case (x, y, z) if x == y && y == z => equilateral
