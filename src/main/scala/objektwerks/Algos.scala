ab07515ce07898271bc3adffcd5f5bcf3d87e8be
objektwerks <objektwerks@runbox.com>
2023-01-30 05:34:58 -0500

    removed triangle from algos

--- src/main/scala/objektwerks/Algos.scala
+++ src/main/scala/objektwerks/Algos.scala
@@ -109,17 +109,3 @@ object Algos:
         loop(currentLevel + 1, topTriangle ++ bottomTriangles)
 
     loop(0, List("*")).mkString("\n")
-
-  object Triangle:
-    trait Kind
-    case object scalene extends Kind     // 0 sides equal
-    case object isoceles extends Kind    // 2 sides equal
-    case object equilateral extends Kind // 3 sides equal
-
-  case class Triangle(a: Int, b: Int, c: Int):
-    import Triangle._
-    def kind: Kind =
-      (a, b, c) match
-        case (x, y, z) if x == y && y == z => equilateral
-        case (x, y, z) if x == y || y == z || z == x => isoceles
-        case _ => scalene
