a120f544abdf8198c0babf1542aad674051e265d
objektwerks <objektwerks@runbox.com>
2023-01-30 05:49:52 -0500

    corrected justify text app

--- src/main/scala/objektwerks/app/JustifyTextApp.scala
+++ src/main/scala/objektwerks/app/JustifyTextApp.scala
@@ -1,9 +1,8 @@
 package objektwerks.app
 
-object JustifyTextApp:
-  import objektwerks.Strings._
+  import objektwerks.Strings.*
 
+object JustifyTextApp:
   val text ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel urna bibendum, pharetra mi quis, imperdiet nibh. Praesent dictum odio lacus, eget commodo sem aliquam rutrum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec eget rhoncus mauris, quis vehicula mi. Quisque finibus purus non varius dictum. Pellentesque vulputate fringilla egestas. Nunc eleifend ex sed egestas cursus. Praesent molestie nisl in pretium vehicula. Vestibulum efficitur ut risus quis porta. Praesent non sem quam. Donec vitae arcu sapien. Quisque aliquet nibh in metus efficitur ullamcorper. Donec mattis dapibus nisl sed iaculis. Curabitur eu blandit enim. Fusce varius."
 
-  @main def runJustifyText(): Unit =
-    println(justify(text, 40))
+  @main def runJustifyText(): Unit = println(justify(text, 40))
