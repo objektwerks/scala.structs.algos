231277f0da4db5c6dba5de75538c709e75ecc016
objektwerks <objektwerks@runbox.com>
2023-01-30 06:16:21 -0500

    edited strings test

--- src/test/scala/objektwerks/StringsTest.scala
+++ src/test/scala/objektwerks/StringsTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 import Strings.*
-e
+
 class StringsTest extends AnyFunSuite with Matchers:
   test("reverse words") {
     reverseWords("scala hello") shouldBe "hello scala"
