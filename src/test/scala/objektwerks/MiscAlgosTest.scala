33661625d6dc16eb72e58483805632c66bac029c
objektwerks <objektwerks@runbox.com>
2023-03-26 14:33:24 -0400

    edited find max profit test

--- src/test/scala/objektwerks/MiscAlgosTest.scala
+++ src/test/scala/objektwerks/MiscAlgosTest.scala
@@ -16,8 +16,8 @@ class MiscAlgosTest extends AnyFunSuite with Matchers:
   }
 
   test("find max profit") {
-    findMaxProfit(Array(163, 112, 105, 100, 151)).get shouldBe 51
-    findMaxProfit(Array(1)).isEmpty shouldBe true
-    findMaxProfit(Array(1, 2)).get shouldBe 1
-    findMaxProfit(Array(2, 1)).isEmpty shouldBe true
+    findMaxProfit(List(163, 112, 105, 100, 151)).get shouldBe 51
+    findMaxProfit(List(1)).isEmpty shouldBe true
+    findMaxProfit(List(1, 2)).get shouldBe 1
+    findMaxProfit(List(2, 1)).isEmpty shouldBe true
   }
