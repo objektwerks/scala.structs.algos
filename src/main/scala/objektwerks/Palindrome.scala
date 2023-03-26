83b8a6c20e78c6b5314d4501f707c1a49b778478
objektwerks <objektwerks@runbox.com>
2023-03-26 09:35:44 -0400

    edited palindrome

--- src/main/scala/objektwerks/Palindrome.scala
+++ src/main/scala/objektwerks/Palindrome.scala
@@ -2,8 +2,8 @@ package objektwerks
 
 /**
   * A palindrome is a sequence of items that are identical when reversed.
-  * For instance, 'aabb' is not a palindrome, nor 'ababb'.
-  * But 'aba' and 'abba' are a palindrome.
+  * For instance, 'aabb' is not a palindrome.
+  * But 'aba' is a palindrome.
   */
 object Palindrome:
   def isPalindrome[T](list: List[T]): Boolean =
