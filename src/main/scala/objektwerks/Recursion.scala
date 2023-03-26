780502f6f2bd30737b12b844de0c5048c23677a9
objektwerks <objektwerks@runbox.com>
2023-03-26 14:17:54 -0400

    removed misc algos from recursion

--- src/main/scala/objektwerks/Recursion.scala
+++ src/main/scala/objektwerks/Recursion.scala
@@ -55,30 +55,3 @@ object Recursion:
       case head :: tail =>
         if listB.contains(head) then intersectLists(tail, listB, acc :+ head)
         else intersectLists(tail, listB, acc)
-
-  def findUnpairedItem[T](list: List[T]): Option[T] =
-    list.foldLeft[Set[T]](Set.empty) {
-      case (set, t) if set.contains(t) => set - t
-      case (set, t) => set + t
-    }
-    .headOption
-
-  def findMaxProfit(stockPrices: Array[Int]): Option[Int] =
-    val maxSellPricesFromIonward = stockPrices
-      .scanRight(0) {
-        case (maximumPriceSoFar, dayPrice) => Math.max(maximumPriceSoFar, dayPrice)
-      }
-      .toArray
-    val maxSellPricesAfterI = maxSellPricesFromIonward.drop(1)
-    if stockPrices.length < 2 then None
-    else
-      stockPrices
-        .zip(maxSellPricesAfterI)
-        .map {
-          case (buyPrice, sellPrice) => getPotentialProfit(buyPrice = buyPrice, sellPrice = sellPrice)
-        }
-        .max
-
-  private def getPotentialProfit(buyPrice: Int, sellPrice: Int): Option[Int] =
-    if sellPrice > buyPrice then Some(sellPrice - buyPrice)
-    else None
