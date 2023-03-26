298fe954babf145374273129171064187d655c28
objektwerks <objektwerks@runbox.com>
2023-03-26 14:32:04 -0400

    edited find max profit

--- src/main/scala/objektwerks/MiscAlgos.scala
+++ src/main/scala/objektwerks/MiscAlgos.scala
@@ -12,16 +12,17 @@ object MiscAlgos:
   def findMaxProfit(stockPrices: Array[Int]): Option[Int] =
     val maxSellPricesFromIonward = stockPrices
       .scanRight(0) {
-        case (maximumPriceSoFar, dayPrice) => Math.max(maximumPriceSoFar, dayPrice)
+        case (maxPriceSoFar, dayPrice) => Math.max(maxPriceSoFar, dayPrice)
       }
       .toArray
     val maxSellPricesAfterI = maxSellPricesFromIonward.drop(1)
+
     if stockPrices.length < 2 then None
     else
       stockPrices
         .zip(maxSellPricesAfterI)
         .map {
-          case (buyPrice, sellPrice) => getPotentialProfit(buyPrice = buyPrice, sellPrice = sellPrice)
+          case (buyPrice, sellPrice) => getPotentialProfit(buyPrice, sellPrice)
         }
         .max
 
