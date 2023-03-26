93f3e3a05efef110e755203d7bffa979dc6d5e21
objektwerks <objektwerks@runbox.com>
2023-03-26 14:07:31 -0400

    enhanced recursion

--- src/main/scala/objektwerks/Recursion.scala
+++ src/main/scala/objektwerks/Recursion.scala
@@ -57,7 +57,28 @@ object Recursion:
         else intersectLists(tail, listB, acc)
 
   def findUnpairedItem[T](list: List[T]): Option[T] =
-    list.foldLeft[Set[T]](Set.empty)({
+    list.foldLeft[Set[T]](Set.empty){
       case (set, t) if set.contains(t) => set - t
       case (set, t) => set + t
-    }).headOption
+    }
+    .headOption
+
+  def findMaxProfit(stockPrices: Array[Int]): Option[Int] =
+    val maxSellPricesFromIonward = stockPrices
+      .scanRight(0){
+        case (maximumPriceSoFar, dayPrice) => Math.max(maximumPriceSoFar, dayPrice)
+      }
+      .toArray
+    val maxSellPricesAfterI = maxSellPricesFromIonward.drop(1)
+    if stockPrices.length < 2 then None
+    else
+      stockPrices
+        .zip(maxSellPricesAfterI)
+        .map {
+          case (buyPrice, sellPrice) => getPotentialProfit(buyPrice = buyPrice, sellPrice = sellPrice)
+        }
+        .max
+
+  private def getPotentialProfit(buyPrice: Int, sellPrice: Int): Option[Int] =
+    if sellPrice > buyPrice then Some(sellPrice - buyPrice)
+    else None
