65363de0baaa00b771951e528d8d99b6a5e96179
objektwerks <objektwerks@runbox.com>
2023-03-26 09:09:53 -0400

    edited fizzbuzz

--- src/main/scala/objektwerks/FizzBuzz.scala
+++ src/main/scala/objektwerks/FizzBuzz.scala
@@ -1,15 +1,16 @@
 package objektwerks
 
 /**
-  * For every number, you should be able to output:
-  * 1. 'Fizz' if it is divisible by 3
-  * 2. 'Buzz', if it is divisible by 5,
-  * 3. 'FizzBuzz' if it is divisible by 3 * 5 (so that the words are combined)
-  * 4. And in all other cases such as '14', return '14', the original number.
+  * For every number input, you should be able to output:
+  * 1. if divisible by 3 return "fizz"
+  * 2. else if divisible by 5 return "buzz"
+  * 3. else if divisible by 15 ( 3 * 5 ) return "fizzbuzz"
+  * 4. else return input number
   */
 object FizzBuzz:
   def apply(n: Int): String =
     n match 
       case i if i % 3 == 0 => "fizz"
       case j if j % 5 == 0 => "buzz"
+      case k if k % 15 == 0 => "fizzbuzz"
       case _ => s"$n"
