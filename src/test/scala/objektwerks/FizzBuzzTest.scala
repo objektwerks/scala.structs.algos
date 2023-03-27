e12cce2fbfc59ae65d5bd1611e38579151330d08
objektwerks <objektwerks@runbox.com>
2023-03-27 17:08:00 -0400

    edited fizzbuzz test

--- src/test/scala/objektwerks/FizzBuzzTest.scala
+++ src/test/scala/objektwerks/FizzBuzzTest.scala
@@ -3,10 +3,10 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+import FizzBuzz.*
+
 class FizzBuzzTest extends AnyFunSuite with Matchers:
   test("fizzbuzz") {
-    import FizzBuzz.*
-
     fizzbuzz(9) shouldBe "fizz"
     fizzbuzz(10) shouldBe "buzz"
     fizzbuzz(15) shouldBe "fizzbuzz"
