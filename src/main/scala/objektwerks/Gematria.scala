2d2625c5127b6a8497fce0403155af0f62641290
objektwerks <objektwerks@runbox.com>
2023-03-28 13:53:22 -0400

    enhanced gematria

--- src/main/scala/objektwerks/Gematria.scala
+++ src/main/scala/objektwerks/Gematria.scala
@@ -3,13 +3,13 @@ package objektwerks
 import scala.collection.immutable.SortedMap
 
 object Gematria:
-  val alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray.map(_.toString).toList
-  val ordinalMap = SortedMap.from( alphabet.zipWithIndex.map( (value, index) => value -> (index + 1) ) )
-  val reverseOrdinalMap = SortedMap.from( alphabet.reverse.zipWithIndex.map( (value, index) => value -> (index + 1) ) )
+  val alphabet = "abcdefghijklmnopqrstuvwxyz".toList
+  val ordinalMap = SortedMap.from( alphabet.zipWithIndex.map( (char, index) => char -> (index + 1) ) )
+  val reverseOrdinalMap = SortedMap.from( alphabet.reverse.zipWithIndex.map( (char, index) => char -> (index + 1) ) )
 
-  def deciper(cipher: SortedMap[String, Int], value: String): Int =
+  def deciper(cipher: SortedMap[Char, Int], value: String): Int =
     value
       .toCharArray
-      .filter(c => c.isLetter)
-      .map(c => cipher.getOrElse(c.toString, 0))
+      .filter(char => char.isLetter)
+      .map(char => cipher.getOrElse(char, 0))
       .sum
