7e556d8ce7687da6167a66d13d5bb97601f492d8
objektwerks <objektwerks@runbox.com>
2023-03-28 13:19:48 -0400

    enhanced gematria

--- src/main/scala/objektwerks/Gematria.scala
+++ src/main/scala/objektwerks/Gematria.scala
@@ -1,5 +1,7 @@
 package objektwerks
 
+import scala.collection.immutable.SortedMap
+
 object Gematria:
   val alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray.map(_.toString).toList
-  val ordinalMap = alphabet.zipWithIndex.map( (value, index) => value -> (index + 1) )
+  val ordinalMap = SortedMap.from( alphabet.zipWithIndex.map( (value, index) => value -> (index + 1) ) )
