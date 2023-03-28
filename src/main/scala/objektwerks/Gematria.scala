570c02db552f27c312ac0ac5cf219c1813aed692
objektwerks <objektwerks@runbox.com>
2023-03-28 12:54:47 -0400

    enhanced gematria

--- src/main/scala/objektwerks/Gematria.scala
+++ src/main/scala/objektwerks/Gematria.scala
@@ -3,4 +3,4 @@ package objektwerks
 
 object Gematria:
   val alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray.map(_.toString).toList
-  val ordinalMap = alphabet.zipWithIndex(1)
+  val ordinalMap = alphabet.zipWithIndex.map( (value, index) => value -> index ).toMap
