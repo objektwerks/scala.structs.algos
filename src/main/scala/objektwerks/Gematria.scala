8d0341d32d336f4f32664247f7e4c9b642e38204
objektwerks <objektwerks@runbox.com>
2023-03-28 13:38:45 -0400

    enhanced gematria

--- src/main/scala/objektwerks/Gematria.scala
+++ src/main/scala/objektwerks/Gematria.scala
@@ -6,3 +6,10 @@ object Gematria:
   val alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray.map(_.toString).toList
   val ordinalMap = SortedMap.from( alphabet.zipWithIndex.map( (value, index) => value -> (index + 1) ) )
   val reverseOrdinalMap = SortedMap.from( alphabet.reverse.zipWithIndex.map( (value, index) => value -> (index + 1) ) )
+
+  def deciper(map: SortedMap[String, Int], value: String): Int =
+    value
+      .toCharArray
+      .filter(c => c.isLetter)
+      .map(c => map.getOrElse(c.toString, 0))
+      .sum
