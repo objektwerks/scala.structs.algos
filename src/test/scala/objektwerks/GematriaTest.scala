e92112ab0a93c7dfeaf05e34dfc4e412b7485c84
objektwerks <objektwerks@runbox.com>
2023-03-28 14:22:49 -0400

    edited gematria test

--- src/test/scala/objektwerks/GematriaTest.scala
+++ src/test/scala/objektwerks/GematriaTest.scala
@@ -8,9 +8,6 @@ import Gematria.*
 class GematriaTest extends AnyFunSuite with Matchers:
   test("ordinal") {
     deciper( ordinalMap, "hello world") shouldBe 124
-    deciper( reverseOrdinalMap, "hello world") shouldBe 146
-    deciper( reductionMap, "hello world") shouldBe 52
-    deciper( reverseReductionMap, "hello world") shouldBe 47
   }
 
   test("reverse ordinal") {
