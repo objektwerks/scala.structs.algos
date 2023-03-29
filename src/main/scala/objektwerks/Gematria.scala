6e12fea9cc2b5022406ad937bc183b6bdf61d791
objektwerks <objektwerks@runbox.com>
2023-03-29 08:17:45 -0400

    edited gematria

--- src/main/scala/objektwerks/Gematria.scala
+++ src/main/scala/objektwerks/Gematria.scala
@@ -40,6 +40,17 @@ object Gematria:
                          'j' -> 600, 'k' -> 10, 'l' -> 20, 'm' -> 30, 'n' -> 40, 'o' -> 50, 'p' -> 60, 'q' -> 70, 'r' -> 80,
                          's' -> 90, 't' -> 100, 'u' -> 200, 'v' -> 700, 'w' -> 900, 'x' -> 300, 'y' -> 400, 'z' -> 500 )
   
+  /* Sumerian
+  a	b	 c	d	 e	f	 g	h	 i	j	 k	l	 m
+  6	12 18	24 30	36 42	48 54	60 66	72 78
+  n	 o	p	 q	 r	 s	 t	 u	 v	 w	 x	 y	 z
+  84 90	96 102 108 114 120 126 132 138 144 150 156
+  */
+
+  /* Reverse Sumerian
+  
+  */
+  
   def deciper(cipher: Map[Char, Int], string: String): Int =
     string
       .toCharArray
