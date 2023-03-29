004dc963a9d74d551fce4223d7b89887ade70c05
objektwerks <objektwerks@runbox.com>
2023-03-29 09:28:14 -0400

    edited gematria test

--- src/test/scala/objektwerks/GematriaTest.scala
+++ src/test/scala/objektwerks/GematriaTest.scala
@@ -6,40 +6,40 @@ import org.scalatest.matchers.should.Matchers
 import Gematria.*
 
 class GematriaTest extends AnyFunSuite with Matchers:
-  val string = "hello world"
+  val text = "hello world"
 
   test("ordinal") {
-    encipher( ordinalCiper, string) shouldBe 124
+    encipher( ordinalCiper, text) shouldBe 124
   }
 
   test("reverse ordinal") {
-    encipher( reverseOrdinalCipher, string) shouldBe 146
+    encipher( reverseOrdinalCipher, text) shouldBe 146
   }
 
   test("reduction") {
-    encipher( reductionCiper, string) shouldBe 52
+    encipher( reductionCiper, text) shouldBe 52
   }
 
   test("reverse reduction") {
-    encipher( reverseReductionCipher, string) shouldBe 47
+    encipher( reverseReductionCipher, text) shouldBe 47
   }
 
   test("standard") {
-    encipher( standardCipher, string) shouldBe 817
+    encipher( standardCipher, text) shouldBe 817
   }
 
   test("reverse standard") {
-    encipher( reverseStandardCipher, string) shouldBe 1253
+    encipher( reverseStandardCipher, text) shouldBe 1253
   }
 
   test("latin") {
-    encipher( latinCipher, string) shouldBe 1157
+    encipher( latinCipher, text) shouldBe 1157
   }
 
   test("sumerian") {
-    encipher( sumerianCipher, string) shouldBe 744
+    encipher( sumerianCipher, text) shouldBe 744
   }
 
   test("reverse sumerian") {
-    encipher( reverseSumerianCipher, string) shouldBe 876
+    encipher( reverseSumerianCipher, text) shouldBe 876
   }
