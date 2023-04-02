13f51b594ce3a98287e7ca37a160949d1172a7d3
objektwerks <objektwerks@runbox.com>
2023-04-02 08:54:23 -0400

    edited gematria

--- src/main/scala/objektwerks/Gematria.scala
+++ src/main/scala/objektwerks/Gematria.scala
@@ -88,6 +88,11 @@ object Gematria:
     Map( 'a' -> 1, 'b' -> 2, 'c' -> 3, 'd' -> 4, 'e' -> 5, 'f' -> 6, 'g' -> 7, 'h' -> 6, 'i' -> 5, 'j' -> 4, 'k' -> 3, 'l' -> 2, 'm' -> 1,
          'n' -> 1, 'o' -> 2, 'p' -> 3, 'q' -> 4, 'r' -> 5, 's' -> 6, 't' -> 7, 'u' -> 6, 'v' -> 5, 'w' -> 4, 'x' -> 3, 'y' -> 2, 'z' -> 1 )
 
+/*
+a	b	c	d	e	f	g	h	i	j	k	l	m	n	o	p	q	r	s	t	u	v	w	x	y	z
+2	2	2	3	3	3	4	4	4	5	5	5	6	6	6	7	7	7	7	8	8	8	9	9	9	9
+*/
+
   def encipher(cipher: Map[Char, Int], text: String): Int =
     text
       .toCharArray
