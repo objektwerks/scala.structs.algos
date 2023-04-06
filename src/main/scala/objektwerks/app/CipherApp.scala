f8719efdab0126366d084a5f7dc98a32319882b8
objektwerks <objektwerks@runbox.com>
2023-04-06 06:13:17 -0400

    enhanced cipher app

--- src/main/scala/objektwerks/app/CipherApp.scala
+++ src/main/scala/objektwerks/app/CipherApp.scala
@@ -1,8 +1,13 @@
 package objektwerks.app
 
 import scalafx.application.JFXApp3
+import scalafx.scene.Scene
 
 object CipherApp extends JFXApp3:
-  override def start(): Unit = ???
-
-  override def stopApp(): Unit = ???
+  override def start(): Unit =
+    stage = new JFXApp3.PrimaryStage:
+      scene = new Scene()
+      title = "Ciphers"
+      minWidth = 800
+      minHeight = 600
+    stage.show()
