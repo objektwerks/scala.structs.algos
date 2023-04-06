3e8f49c12040496de62916e6a8e7f022e688b65a
objektwerks <objektwerks@runbox.com>
2023-04-06 06:25:19 -0400

    added view to cipher app

--- src/main/scala/objektwerks/app/CipherApp.scala
+++ src/main/scala/objektwerks/app/CipherApp.scala
@@ -1,13 +1,23 @@
 package objektwerks.app
 
 import scalafx.application.JFXApp3
+import scalafx.geometry.Insets
 import scalafx.scene.Scene
+import scalafx.scene.layout.BorderPane
 
 object CipherApp extends JFXApp3:
   override def start(): Unit =
     stage = new JFXApp3.PrimaryStage:
-      scene = new Scene()
+      scene = View().scene
       title = "Ciphers"
       minWidth = 800
       minHeight = 600
     stage.show()
+
+class View:
+  val borderPane = new BorderPane:
+    prefWidth = 800
+    prefHeight = 600
+    padding = Insets(6)
+  val scene = new Scene:
+    root = borderPane
