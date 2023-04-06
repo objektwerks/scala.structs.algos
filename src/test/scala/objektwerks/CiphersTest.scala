864da4bf32c773f4ad0cb23f0adfe8342825aa7b
objektwerks <objektwerks@runbox.com>
2023-04-06 07:33:57 -0400

    edited ciphers test

--- src/test/scala/objektwerks/CiphersTest.scala
+++ src/test/scala/objektwerks/CiphersTest.scala
@@ -9,7 +9,7 @@ class CiphersTest extends AnyFunSuite with Matchers:
   val text = "hello world"
 
   test("ordinal") {
-    encipher( ordinalCiper, text) shouldBe 124
+    encipher( ordinalCipher, text) shouldBe 124
   }
 
   test("reverse ordinal") {
@@ -17,7 +17,7 @@ class CiphersTest extends AnyFunSuite with Matchers:
   }
 
   test("reduction") {
-    encipher( reductionCiper, text) shouldBe 52
+    encipher( reductionCipher, text) shouldBe 52
   }
 
   test("reverse reduction") {
@@ -45,47 +45,47 @@ class CiphersTest extends AnyFunSuite with Matchers:
   }
 
   test("primes") {
-    encipher( primesCiper, text) shouldBe 386
+    encipher( primesCipher, text) shouldBe 386
   }
 
   test("reverse primes") {
-    encipher( reversePrimesCiper, text) shouldBe 474
+    encipher( reversePrimesCipher, text) shouldBe 474
   }
 
   test("fibonacci") {
-    encipher( fibonacciCiper, text) shouldBe 786
+    encipher( fibonacciCipher, text) shouldBe 786
   }
 
   test("squares") {
-    encipher( squaresCiper, text) shouldBe 1840
+    encipher( squaresCipher, text) shouldBe 1840
   }
 
   test("reverse squares") {
-    encipher( reverseSquaresCiper, text) shouldBe 2434
+    encipher( reverseSquaresCipher, text) shouldBe 2434
   }
 
   test("trigonal") {
-    encipher( trigonalCiper, text) shouldBe 982
+    encipher( trigonalCipher, text) shouldBe 982
   }
 
   test("reverse trigonal") {
-    encipher( reverseTrigonalCiper, text) shouldBe 1290
+    encipher( reverseTrigonalCipher, text) shouldBe 1290
   }
 
   test("single reduction") {
-    encipher( singleReductionCiper, text) shouldBe 52
+    encipher( singleReductionCipher, text) shouldBe 52
   }
 
   test("reverse single reduction") {
-    encipher( reverseSingleReductionCiper, text) shouldBe 56
+    encipher( reverseSingleReductionCipher, text) shouldBe 56
   }
 
   test("keypad") {
-    encipher( keypadCiper, text) shouldBe 53
+    encipher( keypadCipher, text) shouldBe 53
   }
 
   test("chaldean") {
-    encipher( chaldeanCiper, text) shouldBe 45
+    encipher( chaldeanCipher, text) shouldBe 45
   }
 
   test("septenary") {
@@ -93,29 +93,29 @@ class CiphersTest extends AnyFunSuite with Matchers:
       1. https://grahamhancock.com/leedsm1/
       2. https://truthscrambler.com/2019/03/24/the-predominance-and-occultation-of-the-septenary-cypher-marty-leeds/
     */
-    encipher( septenaryCiper, "lord") shouldBe 13
-    encipher( septenaryCiper, "god") shouldBe 13
-    encipher( septenaryCiper, "lordgod") shouldBe 26
-    encipher( septenaryCiper, "adameve") shouldBe 22
-    encipher( septenaryCiper, "jesus") shouldBe 27 // 3 x 3 x 3
+    encipher( septenaryCipher, "lord") shouldBe 13
+    encipher( septenaryCipher, "god") shouldBe 13
+    encipher( septenaryCipher, "lordgod") shouldBe 26
+    encipher( septenaryCipher, "adameve") shouldBe 22
+    encipher( septenaryCipher, "jesus") shouldBe 27 // 3 x 3 x 3
 
     val dice = "onetwothreefourfivesix"
-    encipher( septenaryCiper, dice) shouldBe 103 // 103
-    encipher( septenaryCiper, dice + dice) shouldBe 206 // 103 * 2 = 206, number of bones in a human skeleton
+    encipher( septenaryCipher, dice) shouldBe 103 // 103
+    encipher( septenaryCipher, dice + dice) shouldBe 206 // 103 * 2 = 206, number of bones in a human skeleton
 
-    encipher( septenaryCiper, "abcdef") shouldBe 21
-    encipher( septenaryCiper, "g") shouldBe 7
-    encipher( septenaryCiper, "hijklm") shouldBe 21
+    encipher( septenaryCipher, "abcdef") shouldBe 21
+    encipher( septenaryCipher, "g") shouldBe 7
+    encipher( septenaryCipher, "hijklm") shouldBe 21
 
-    encipher( septenaryCiper, "nopqrs") shouldBe 21
-    encipher( septenaryCiper, "t") shouldBe 7
-    encipher( septenaryCiper, "uvwxyz") shouldBe 21
+    encipher( septenaryCipher, "nopqrs") shouldBe 21
+    encipher( septenaryCipher, "t") shouldBe 7
+    encipher( septenaryCipher, "uvwxyz") shouldBe 21
 
-    encipher( septenaryCiper, "seven") shouldBe 22 // divided by 7 equals 3.14 pi
+    encipher( septenaryCipher, "seven") shouldBe 22 // divided by 7 equals 3.14 pi
  }
 
   test("encipher to map") {
-    encipherToMap( ordinalCiper, text) shouldBe List(('h',8), ('e',5), ('l',12), ('l',12), ('o',15), ('w',23), ('o',15), ('r',18), ('l',12), ('d',4))
-    encipherToMap( ordinalCiper, text).map( (l, i) => l).mkString shouldBe "helloworld"
-    encipherToMap( ordinalCiper, text).map( (l, i) => i).mkString shouldBe "85121215231518124"
+    encipherToMap( ordinalCipher, text) shouldBe List(('h',8), ('e',5), ('l',12), ('l',12), ('o',15), ('w',23), ('o',15), ('r',18), ('l',12), ('d',4))
+    encipherToMap( ordinalCipher, text).map( (l, i) => l).mkString shouldBe "helloworld"
+    encipherToMap( ordinalCipher, text).map( (l, i) => i).mkString shouldBe "85121215231518124"
   }
