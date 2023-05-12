000298aa064297c04dfda9dd95f9ffe22e4672ad
objektwerks <objektwerks@runbox.com>
2023-05-12 15:22:21 -0400

    simplified n queens app

--- src/main/scala/objektwerks/cli/NQueensApp.scala
+++ src/main/scala/objektwerks/cli/NQueensApp.scala
@@ -2,8 +2,7 @@ package objektwerks.cli
 
 import objektwerks.NQueens.*
 
-object NQueensApp:
-  @main def runNQueens(): Unit =
-    val solutions = nQueens()
-    println(solutions.mkString("\n\n"))
-    println(s"Total solutions: ${solutions.length}")
+@main def runNQueens(): Unit =
+  val solutions = nQueens()
+  println(solutions.mkString("\n\n"))
+  println(s"Total solutions: ${solutions.length}")
