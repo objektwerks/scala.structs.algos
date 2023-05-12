29548f210ec20de0a5d9a436380a6df276c7d3fe
objektwerks <objektwerks@runbox.com>
2023-05-12 15:20:48 -0400

    simplified eval app

--- src/main/scala/objektwerks/cli/EvalApp.scala
+++ src/main/scala/objektwerks/cli/EvalApp.scala
@@ -2,10 +2,9 @@ package objektwerks.cli
 
 import objektwerks.Eval.*
 
-object EvalApp:
-  @main def runEval(): Unit =
-    println(s"1 + 2 = ${eval("1 + 2")}")
-    println(s"4 - 1 = ${eval("4 - 1")}")
-    println(s"1 * 3 = ${eval("1 * 3")}")
-    println(s"9 / 3 = ${eval("9 / 3")}")
-    println(s"1 + 2 * 3 + 4 / 5 + 6 * 7 - 8 = ${eval("1 + 2 * 3 + 4 / 5 + 6 * 7 - 8")}")
+@main def runEval(): Unit =
+  println(s"1 + 2 = ${eval("1 + 2")}")
+  println(s"4 - 1 = ${eval("4 - 1")}")
+  println(s"1 * 3 = ${eval("1 * 3")}")
+  println(s"9 / 3 = ${eval("9 / 3")}")
+  println(s"1 + 2 * 3 + 4 / 5 + 6 * 7 - 8 = ${eval("1 + 2 * 3 + 4 / 5 + 6 * 7 - 8")}")
