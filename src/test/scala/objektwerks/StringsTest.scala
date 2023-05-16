fd2faa939bab4cb16450d6494b856bff54ce298c
objektwerks <objektwerks@runbox.com>
2023-05-16 16:03:08 -0400

    refactored strings test

--- src/test/scala/objektwerks/StringsTest.scala
+++ src/test/scala/objektwerks/StringsTest.scala
@@ -5,8 +5,6 @@ import org.scalatest.matchers.should.Matchers
 
 import Strings.*
 
-import objektwerks.cli.JustifyTextApp
-
 class StringsTest extends AnyFunSuite with Matchers:
   test("reverse words") {
     reverseWords("scala hello") shouldBe "hello scala"
@@ -38,5 +36,6 @@ class StringsTest extends AnyFunSuite with Matchers:
   }
 
   test("justifyText") {
-    justify(JustifyTextApp.text, 40).nonEmpty shouldBe true
+    val text ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel urna bibendum, pharetra mi quis, imperdiet nibh. Praesent dictum odio lacus, eget commodo sem aliquam rutrum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec eget rhoncus mauris, quis vehicula mi. Quisque finibus purus non varius dictum. Pellentesque vulputate fringilla egestas. Nunc eleifend ex sed egestas cursus. Praesent molestie nisl in pretium vehicula. Vestibulum efficitur ut risus quis porta. Praesent non sem quam. Donec vitae arcu sapien. Quisque aliquet nibh in metus efficitur ullamcorper. Donec mattis dapibus nisl sed iaculis. Curabitur eu blandit enim. Fusce varius."
+    justify(text, 40).nonEmpty shouldBe true
   }
