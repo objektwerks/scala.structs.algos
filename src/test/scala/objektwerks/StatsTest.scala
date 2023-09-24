cff675d550fe5b2fe6a1e51514b72ec8892ccc9f
objektwerks <objektwerks@runbox.com>
2023-09-24 08:42:35 -0400

    reformatted test

--- src/test/scala/objektwerks/StatsTest.scala
+++ src/test/scala/objektwerks/StatsTest.scala
@@ -5,76 +5,58 @@ import org.scalatest.matchers.should.Matchers
 
 import Stats.*
 
-class StatsTest extends AnyFunSuite with Matchers {
+final class StatsTest extends AnyFunSuite with Matchers:
   val xs = Vector[Double](1.0, 2.0, 3.0, 3.0, 4.0, 4.0, 5.0)
   val ys = Vector[Double](1.5, 2.5, 3.25, 3.5, 4.25, 4.75, 5.5)
   val outcomesToCount = Vector((1.0, 5.0), (2.0, 10.0), (3.0, 11.0), (4.0, 44.0), (5.0, 38.0))
 
-  test("mean") {
+  test("mean"):
     mean(xs) shouldBe 3.142857142857143
-  }
 
-  test("median") {
+  test("median"):
     median(xs) shouldBe 3.0
-  }
 
-  test("mode") {
+  test("mode"):
     mode(xs) shouldBe Vector(3.0, 4.0)
-  }
 
-  test("range") {
+  test("range"):
     range(xs) shouldBe 4.0
-  }
 
-  test("histogram") {
+  test("histogram"):
     histogram(xs) shouldBe Map(1.0 -> 1, 2.0 -> 1, 3.0 -> 2, 4.0 -> 2, 5.0 -> 1)
-  }
 
-  test("variance") {
+  test("variance"):
     variance(xs) shouldBe 1.8095238095238093
-  }
 
-  test("standard deviation") {
+  test("standard deviation"):
     standardDeviation(xs) shouldBe 1.3451854182690985
-  }
 
-  test("standard error") {
+  test("standard error"):
     standardError(xs) shouldBe 0.5084322977157767
-  }
 
-  test("covariance") {
+  test("covariance"):
     covariance(xs, ys) shouldBe 1.8154761904761905
-  }
 
-  test("correlation coefficient") {
+  test("correlation coefficient"):
     correlationCoefficient(xs, ys) shouldBe 0.9919341745639146
-  }
 
-  test("centroid") {
+  test("centroid"):
     (3.142857142857143, 3.607142857142857) shouldBe centroid(xs, ys)
-  }
 
-  test("factorial") {
+  test("factorial"):
     factorial(9) shouldBe 362880
-  }
 
-  test("fibonacci") {
+  test("fibonacci"):
     fibonacci(39) shouldBe 63245986
-  }
 
-  test("combinations") {
+  test("combinations"):
     combinations(10, 3) shouldBe 120
-  }
 
-  test("permutations") {
+  test("permutations"):
     permutations(10, 3) shouldBe 720
-  }
 
-  test("probability") {
+  test("probability"):
     probability(outcomesToCount) shouldBe Vector(0.046296296296296294, 0.09259259259259259, 0.10185185185185185, 0.4074074074074074, 0.35185185185185186)
-  }
 
-  test("expected value") {
+  test("expected value"):
     expectedValue(outcomesToCount) shouldBe 3.9259259259259256
-  }
-}
