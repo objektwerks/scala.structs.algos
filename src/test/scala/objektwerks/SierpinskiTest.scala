ba4b458759d834780d7225cb2999dea2fa854272
objektwerks <objektwerks@runbox.com>
2023-09-24 08:40:37 -0400

    reformatted test

--- src/test/scala/objektwerks/SierpinskiTest.scala
+++ src/test/scala/objektwerks/SierpinskiTest.scala
@@ -5,9 +5,8 @@ import org.scalatest.matchers.should.Matchers
 
 import Sierpinski.*
 
-class SierpinskiTest extends AnyFunSuite with Matchers:
-  test("sierpinski") {
+final class SierpinskiTest extends AnyFunSuite with Matchers:
+  test("sierpinski"):
     sierpinski(1).length shouldBe 7
     sierpinski(2).length shouldBe 31
     sierpinski(3).length shouldBe 127
-  }
