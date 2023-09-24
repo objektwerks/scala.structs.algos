f3ff6a12fbb4386ff2234b5b7a71ef971e9c7217
objektwerks <objektwerks@runbox.com>
2023-09-24 08:19:59 -0400

    reformatted test

--- src/test/scala/objektwerks/AsymmetricCryptoTest.scala
+++ src/test/scala/objektwerks/AsymmetricCryptoTest.scala
@@ -3,8 +3,8 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class AsymmetricCryptoTest extends AnyFunSuite with Matchers:
-  test("encrypt > decrypt") {
+final class AsymmetricCryptoTest extends AnyFunSuite with Matchers:
+  test("encrypt > decrypt"):
     val text = "Dogfishhead 60' IPA is the best IPA in the world!"
     println(s"asymmetric text: $text")
 
@@ -16,4 +16,3 @@ class AsymmetricCryptoTest extends AnyFunSuite with Matchers:
       println(s"asymmetric encrypted text: $encryptedText")
       println(s"asymmetric decrypted text: $decryptedText")
       text shouldBe decryptedText
-  }
