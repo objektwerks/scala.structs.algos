95762492a810f7c58519b9202394aec3bce61a71
objektwerks <objektwerks@runbox.com>
2023-09-24 08:46:33 -0400

    reformatted test

--- src/test/scala/objektwerks/TreesTest.scala
+++ src/test/scala/objektwerks/TreesTest.scala
@@ -5,7 +5,7 @@ import org.scalatest.matchers.should.Matchers
 
 import Trees.*
 
-class TreesTest extends AnyFunSuite with Matchers:
+final class TreesTest extends AnyFunSuite with Matchers:
   val tree = BNode(1,
     BNode(2,
       BNode(3, BEnd, BEnd),
@@ -44,46 +44,39 @@ class TreesTest extends AnyFunSuite with Matchers:
       BNode(8, BEnd, BEnd)
     )
   )
-  test("leafCount") {
+
+  test("leafCount"):
     tree.leafCount shouldBe 4
-  }
 
-  test("size") {
+  test("size"):
     tree.size shouldBe 8
-  }
 
-  test("collectLeaves") {
+  test("collectLeaves"):
     tree.collectLeaves.map(_.value) shouldBe List(8, 7, 5, 3)
-  }
 
-  test("collectNodes") {
+  test("collectNodes"):
     tree.collectNodes(-1).map(_.value) shouldBe List()
     tree.collectNodes(0).map(_.value) shouldBe List(1)
     tree.collectNodes(1).map(_.value) shouldBe List(2, 6)
     tree.collectNodes(2).map(_.value) shouldBe List(3, 4, 7, 8)
     tree.collectNodes(3).map(_.value) shouldBe List(5)
-  }
 
-  test("mirror") {
+  test("mirror"):
     val mirror = tree.mirror
     mirror.collectNodes(1).map(_.value) shouldBe List(6, 2)
     mirror.collectNodes(2).map(_.value) shouldBe List(8, 7, 4, 3)
-  }
 
-  test("sameShapeAs") {
+  test("sameShapeAs"):
     tree.sameShapeAs(tree) shouldBe true
     tree.sameShapeAs(xtree) shouldBe true
     tree.sameShapeAs(tree.mirror) shouldBe false
-  }
 
-  test("isSymmetrical") {
+  test("isSymmetrical"):
     symmetricalTree.isSymmetrical shouldBe true
     tree.isSymmetrical shouldBe false
     tree.mirror.isSymmetrical shouldBe false
-  }
 
-  test("toList") {
+  test("toList"):
     tree.toList shouldBe List(1, 2, 6, 3, 4, 7, 8, 5)
     xtree.toList shouldBe List(10, 20, 60, 30, 40, 70, 80, 50)
     symmetricalTree.toList shouldBe List(1, 2, 6, 3, 4, 7, 8)
-  }
