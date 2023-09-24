ff8a026fbb950ddc85af8df5dc74ed951cbf168f
objektwerks <objektwerks@runbox.com>
2023-09-24 08:36:42 -0400

    reformatted test

--- src/test/scala/objektwerks/PinTest.scala
+++ src/test/scala/objektwerks/PinTest.scala
@@ -3,7 +3,6 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class PinTest extends AnyFunSuite with Matchers:
-  test("pin") {
+final class PinTest extends AnyFunSuite with Matchers:
+  test("pin"):
     Pin().length shouldBe 7
-  }
