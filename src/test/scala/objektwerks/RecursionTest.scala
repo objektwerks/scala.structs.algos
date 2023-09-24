dd681cf1fab06edf7bde8a29348d5b810e8046e0
objektwerks <objektwerks@runbox.com>
2023-09-24 08:38:05 -0400

    reformatted test

--- src/test/scala/objektwerks/RecursionTest.scala
+++ src/test/scala/objektwerks/RecursionTest.scala
@@ -5,39 +5,32 @@ import org.scalatest.matchers.should.Matchers
 
 import Recursion.*
 
-class RecursionTest extends AnyFunSuite with Matchers:
-  test("sum") {
+final class RecursionTest extends AnyFunSuite with Matchers:
+  test("sum"):
     sum( Nil ) shouldBe 0
     sum( List(1, 2, 3) ) shouldBe 6
-  }
 
-  test("product") {
+  test("product"):
     product( Nil ) shouldBe 1
     product( List(1, 2, 3) ) shouldBe 6
-  }
 
-  test("reverse") {
+  test("reverse"):
     reverse( List(1, 2, 3) ) shouldBe List(3, 2, 1)
-  }
 
-  test("find nth element from right") {
+  test("find nth element from right"):
     val xs = (1 to 10).toList
     findNthElementFromRight(xs, 4) shouldBe Some(7)
     findNthElementFromRight(xs, 15) shouldBe None
-  }
 
-  test("factorial") {
+  test("factorial"):
     factorial(3) shouldBe 6
-  }
 
-  test("fibbonaci") {
+  test("fibbonaci"):
     fibonacci(39) shouldBe 63245986
-  }
 
-  test("intersect lists") {
+  test("intersect lists"):
     val listA = (1 to 10).toList
     val listB = (5 to 15).toList
     val listIntersection = List(5, 6, 7, 8, 9, 10)
     intersectLists(listA, listB) shouldBe listIntersection
     ( listA intersect listB ) shouldBe listIntersection
-  }
