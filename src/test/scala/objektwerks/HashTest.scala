0415fea940a6fd7c7f0ef102cbe2758ffd942f68
objektwerks <objektwerks@runbox.com>
2023-09-24 08:30:42 -0400

    reformatted test

--- src/test/scala/objektwerks/HashTest.scala
+++ src/test/scala/objektwerks/HashTest.scala
@@ -3,11 +3,10 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class HashTest extends AnyFunSuite with Matchers:
-  test("hash") {
+final class HashTest extends AnyFunSuite with Matchers:
+  test("hash"):
     val text = "Dogfishhead 60' IPA is the best IPA in the world!"
     val hash = Hash.sha3256(text)
     hash.length shouldBe 64
     hash shouldBe Hash.sha3256(text)
     hash should not be Hash.sha3256(text + "!!")
-  }
