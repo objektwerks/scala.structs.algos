bb34ae6ce21ff78a59ed5eb237ca285fedf7ba45
objektwerks <objektwerks@runbox.com>
2023-09-24 08:46:59 -0400

    reformatted test

--- src/test/scala/objektwerks/TriangleTest.scala
+++ src/test/scala/objektwerks/TriangleTest.scala
@@ -5,9 +5,8 @@ import org.scalatest.matchers.should.Matchers
 
 import Triangle.*
 
-class TriangleTest extends AnyFunSuite with Matchers:
-  test("triangles") {
+final class TriangleTest extends AnyFunSuite with Matchers:
+  test("triangles"):
     Triangle(3, 6, 9).kind shouldEqual Triangle.scalene
     Triangle(3, 6, 3).kind shouldEqual Triangle.isoceles
     Triangle(3, 3, 3).kind shouldEqual Triangle.equilateral
-  }
