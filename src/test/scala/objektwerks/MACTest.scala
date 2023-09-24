e55f1b7de8c81c8f66123a0f536b11804d2d3123
objektwerks <objektwerks@runbox.com>
2023-09-24 08:31:37 -0400

    reformatted test

--- src/test/scala/objektwerks/MACTest.scala
+++ src/test/scala/objektwerks/MACTest.scala
@@ -3,21 +3,17 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class MACTest extends AnyFunSuite with Matchers:
+final class MACTest extends AnyFunSuite with Matchers:
   private val address = "50ed3c45f4ba"
 
-  test("valid mac") {
+  test("valid mac"):
     MAC(address).foreach(a => a shouldBe address)
-  }
 
-  test("invalid mac") {
+  test("invalid mac"):
     MAC("1a2b3c").foreach(a => a shouldBe "")
-  }
 
-  test("display mac") {
+  test("display mac"):
     MAC(address).foreach(a => a.display shouldBe "50:ed:3c:45:f4:ba")
-  }
 
-  test("number mac") {
+  test("number mac"):
     MAC(address).foreach(a => a.number shouldBe 88979848688826L)
-  }
