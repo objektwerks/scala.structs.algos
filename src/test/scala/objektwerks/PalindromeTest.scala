f234d88b761572fdc595abc694d87fee2193c16c
objektwerks <objektwerks@runbox.com>
2023-09-24 08:36:14 -0400

    reformatted test

--- src/test/scala/objektwerks/PalindromeTest.scala
+++ src/test/scala/objektwerks/PalindromeTest.scala
@@ -5,8 +5,7 @@ import org.scalatest.matchers.should.Matchers
 
 import Palindrome.*
 
-class PalindromeTest extends AnyFunSuite with Matchers:
-  test("palindrome") {
+final class PalindromeTest extends AnyFunSuite with Matchers:
+  test("palindrome"):
     isPalindrome( List('a', 'a', 'b', 'b')) shouldBe false
     isPalindrome( List('a', 'b', 'a')) shouldBe true
-  }
