861d0fba7d56738bb10d156cd86cbab188db234a
Objektwerks <objektwerks@runbox.com>
2024-07-26 13:10:14 -0400

    Update Pin.scala

--- src/main/scala/objektwerks/Pin.scala
+++ src/main/scala/objektwerks/Pin.scala
@@ -4,21 +4,14 @@ import scala.util.Random
 
 object Pin:
   private val specialChars = "~!@#$%^&*-+=<>?/:;".toList
-  private val limit = specialChars.length
-  private val random = new Random
+  private val random = Random
 
-  private def newSpecialChar: Char = specialChars(random.nextInt(limit))
+  private def newSpecialChar: Char = specialChars( random.nextInt(specialChars.length) )
 
-  /**
-   * 26 letters + 10 numbers + 18 special characters = 54 combinations
-   * 7 alphanumeric char pin = 54^7 ( 1,338,925,209,984 )
-   */
-  def apply(): String =
-    Random.shuffle(
-      Random
-        .alphanumeric
-        .take(5)
-        .mkString
-        .prepended(newSpecialChar)
-        .appended(newSpecialChar)
-    ).mkString
+  def newInstance: String =
+    Random
+      .alphanumeric
+      .take(5)
+      .mkString
+      .prepended(newSpecialChar)
+      .appended(newSpecialChar)
